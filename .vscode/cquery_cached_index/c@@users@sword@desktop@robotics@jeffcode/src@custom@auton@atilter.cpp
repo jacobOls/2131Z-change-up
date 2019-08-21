20cf07e2acefbe1afd8b6f491a34102218aaa862
swordsparring <swordsparring@gmail.com>
2019-08-21 16:22:02 -0600

    dc

---
+++ .vscode/cquery_cached_index/c@@users@sword@desktop@robotics@jeffcode/src@custom@auton@atilter.cpp
@@ -0,0 +1,3 @@
+#include "main.h"
+#include "custom/systems/tilter.hpp"
+#include "custom/auton/atilter.hpp"
---
+++ src/custom/auton/alift.cpp
@@ -0,0 +1,3 @@
+#include "main.h"
+#include "custom/systems/lift.hpp"
+#include "custom/auton/alift.hpp"

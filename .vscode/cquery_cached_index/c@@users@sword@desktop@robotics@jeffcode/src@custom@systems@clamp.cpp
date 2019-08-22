5fcbf15718653f7b09d186b778cc41db6a6baf46
swordsparring <swordsparring@gmail.com>
2019-08-22 16:36:49 -0600

    more more more comments :smile:

--- .vscode/cquery_cached_index/c@@users@sword@desktop@robotics@jeffcode/src@custom@systems@clamp.cpp
+++ .vscode/cquery_cached_index/c@@users@sword@desktop@robotics@jeffcode/src@custom@systems@clamp.cpp
@@ -2,27 +2,23 @@
 #include "custom/systems/clamp.hpp"
 #include "custom/setup/controller.hpp"
 #include "custom/setup/motors.hpp"
-bool clampOpen = false;
+bool clampOpen = false; //is the clamp open or closed
 namespace clamp{
   void clampdump(){
-
-    if(!clampOpen)
+    if(!clampOpen)//if its closed run this next code
     {
       if(BtnToggle.isPressed())
       {
         clampOpen = true;
         clampMotor.moveAbsolute(180,100);
       }
-
     }
-    else
+    else//if open do this
     {
       if(BtnToggle.isPressed())
       {
-
         clampOpen = false;
         clampMotor.moveAbsolute(0,-100);
-
       }
     }
   }

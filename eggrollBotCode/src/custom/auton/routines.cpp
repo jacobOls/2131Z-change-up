30e51b9229bc1ace6f648e4bfe0a35f04ec86498
Jacob <swordsparring@gmail.com>
2019-09-08 09:23:40 -0600

    testing autons

--- eggrollBotCode/src/custom/auton/routines.cpp
+++ eggrollBotCode/src/custom/auton/routines.cpp
@@ -1,6 +1,9 @@
 #include "custom/auton/routines.hpp"
 #include "custom/systems/drive.hpp"
 #include "custom/setup/controller.hpp"
+#include "custom/systems/tray.hpp"
+#include "custom/systems/lift.hpp"
+#include "custom/systems/intake.hpp"
 #include "main.h"
 namespace auton
 {
@@ -8,9 +11,12 @@ void driveTest()
 {
 drive::auton::autonDrive(3000, 40);
 pros::delay(1000);
+intake::auton::intakeOn(100);
+pros::delay(1000);
 drive::auton::turn(400,50);
 pros::delay(1000);
-
+intake::auton::intakeOff();
+pros::delay(1000);
 }
 
 

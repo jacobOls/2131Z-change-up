b6d5666df532c29391356bdfd29786f8bf0e8cb0
Jacob <swordsparring@gmail.com>
2019-10-23 16:18:19 -0600

    autons

--- eggrollBotCode/src/custom/auton/routines.cpp
+++ eggrollBotCode/src/custom/auton/routines.cpp
@@ -15,27 +15,33 @@ namespace auton{
   void skillsAuton(){
     tray::motor.tarePosition();
     drive::auton::resetPositions();
-    drive::auton::autonDrive(300,50);
-    drive::auton::autonDriveBack(-310,-50);
+    drive::auton::autonDrive(300,75);
     lift::auton::popOpen();
+    drive::auton::autonDriveBack(-290,-125);
     lift::auton::autonLiftDown(2380,-100);
     tray::auton::trayDown(0,-100);
     intake::auton::intakeOn(200);
-    drive::auton::autonDriveBack(-30,-50);
-    drive::auton::autonDrive(1100,50);
-    drive::auton::autonDriveBack(-300,-50);
-    drive::auton::leftTurn(8,50);
-    drive::auton::autonDrive(450,50);
-    drive::auton::autonDriveBack(-225,-50);
-    drive::auton::rightTurn(5,50);
-    drive::auton::autonDrive(500,50);
-    drive::auton::autonDrive(-100,50);
+    drive::auton::autonDriveBack(-10, -50);
+    drive::auton::autonDrive(1000,70);
+    drive::auton::autonDriveBack(-175,-125);
+    drive::auton::leftTurn(3,25);
+    drive::auton::autonDrive(450,60);
+    wait(200);
+    drive::auton::autonDriveBack(-250,-150);
+    drive::auton::rightTurn(95,50);
+    drive::auton::autonDrive(390,65);
+    wait(1100);
+    intake::auton::intakeOff();
+    drive::auton::autonDriveBack(-930, -150);
+    drive::auton::rightTurn(180,50);
+    drive::auton::autonDrive(500, 100);
+    intake::auton::intakeOn(-25);
     wait(1000);
     intake::auton::intakeOff();
-    drive::auton::autonDriveBack(-850,-100);
-    drive::auton::rightTurn(300,50);
-    drive::auton::autonDrive(200,50);
-    tray::auton::stack(300,25);
+    tray::auton::stack(400,25);
+    wait(500);
+    intake::auton::stackReverseAuton(-500, 25, 25);
+    tray::auton::trayDown(0,-100);
   }
 
   void redRight(){

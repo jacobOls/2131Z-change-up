d05608cf00b810a140c671266f6586a55cf822e6
Jacob <swordsparring@gmail.com>
2019-10-30 15:23:21 -0600

    drive

---
+++ Gen4/include/custom/systems/drive.hpp
@@ -0,0 +1,9 @@
+#ifndef DRIVE_HPP
+#define DRIVE_HPP
+
+namespace driveP{
+  void drive(void);
+}
+
+
+#endif /* end of include guard: DRIVE_HPP */
---
+++ Gen4/src/custom/systems/drive.cpp
@@ -0,0 +1,24 @@
+#include "main.h"
+#include "custom/setup/motors.hpp"
+#include "custom/setup/controller.hpp"
+#include "custom/systems/drive.hpp"
+
+
+namespace drive{
+
+  int rightVel;
+  int leftVel;
+
+  void drive(){
+    //drive code
+    if(std::abs(master.getAnalog(okapi::ControllerAnalog::leftY))<0.05) leftVel=0;
+    else	leftVel=(master.getAnalog(ControllerAnalog::leftY)*200);
+    if(std::abs(master.getAnalog(okapi::ControllerAnalog::rightY))<0.05) rightVel=0;
+    else	rightVel=(master.getAnalog(ControllerAnalog::rightY)*200);
+
+    right_drive.moveVelocity(rightVel);
+    left_drive.moveVelocity(leftVel);
+
+
+  }
+}

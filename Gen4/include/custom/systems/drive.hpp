8bf06d1b5c9e1b7a12d9ef7862bd2c2e198766ea
Jacob <swordsparring@gmail.com>
2019-11-03 18:25:53 -0700

    formatting

--- Gen4/include/custom/systems/drive.hpp
+++ Gen4/include/custom/systems/drive.hpp
@@ -3,14 +3,14 @@
 
 namespace drive
 {
-  void drive(void);
+void drive(void);
 
-  namespace auton
-  {
-    void driving(int distance, int velocity);
-    void rightTurn(double distance, double velocity);
-    void leftTurn(double distance, double velocity);
-  }
+namespace auton
+{
+void driving(int distance, int velocity);
+void rightTurn(double distance, double velocity);
+void leftTurn(double distance, double velocity);
+} // namespace auton
 } // namespace drive
 
 #endif /* end of include guard: DRIVE_HPP */

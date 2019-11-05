33ba25bfc8163d6033f17dbeb3ce17248b9b69de
Jacob <swordsparring@gmail.com>
2019-11-05 14:42:17 -0700

    formatting

--- Gen4/src/custom/systems/lift.cpp
+++ Gen4/src/custom/systems/lift.cpp
@@ -6,159 +6,159 @@
 
 namespace lift
 {
-Controllers controller = Controllers::NONE;
-double sensor()
-{
-  return liftSensor.get_value();
-}
-double tilter()
-{
-  return tilter::tilterSensor.get_value();
-}
-int upPlace = 1850;
-int sensorPlace = 1350;
-int fastSpeedSpot = 1250;
-int startUp = 1640;
-
-void up()
-{
-  if (BtnUp.isPressed())
+  Controllers controller = Controllers::NONE;
+  double sensor()
   {
-    controller = Controllers::UP;
+    return liftSensor.get_value();
   }
-  else if (controller == Controllers::UP)
+  double tilter()
   {
-    controller = Controllers::DEINIT;
+    return tilter::tilterSensor.get_value();
   }
-}
+  int upPlace = 1850;
+  int sensorPlace = 1350;
+  int fastSpeedSpot = 1250;
+  int startUp = 1640;
 
-void down()
-{
-  if (BtnDown.isPressed())
+  void up()
   {
-    controller = Controllers::DOWN;
-  }
-  else if (controller == Controllers::DOWN)
-  {
-    controller = Controllers::DEINIT;
-  }
-}
-
-
-
-void execute()
-{
-  if (controller == Controllers::UP)
-  {
-    if (sensor() > 2200)
+    if (BtnUp.isPressed())
+    {
+      controller = Controllers::UP;
+    }
+    else if (controller == Controllers::UP)
     {
       controller = Controllers::DEINIT;
     }
   }
-  if (controller == Controllers::DOWN)
+
+  void down()
   {
-    if (sensor() < 1000)
+    if (BtnDown.isPressed())
+    {
+      controller = Controllers::DOWN;
+    }
+    else if (controller == Controllers::DOWN)
     {
       controller = Controllers::DEINIT;
     }
   }
-  switch (controller)
-  {
 
-  case Controllers::UP:
-    if (tilter() < startUp)
+
+
+  void execute()
+  {
+    if (controller == Controllers::UP)
     {
-      tilter::motor.moveVelocity(75);
+      if (sensor() > 2200)
+      {
+        controller = Controllers::DEINIT;
+      }
     }
-    else
+    if (controller == Controllers::DOWN)
     {
-      motor.moveVelocity(75);
-      if (tilter() < upPlace)
+      if (sensor() < 1000)
+      {
+        controller = Controllers::DEINIT;
+      }
+    }
+    switch (controller)
+    {
+
+      case Controllers::UP:
+      if (tilter() < startUp)
       {
         tilter::motor.moveVelocity(75);
       }
-    }
-    break;
-
-  case Controllers::DOWN:
-    if (sensor() > fastSpeedSpot)
-    {
-      motor.moveVelocity(-75);
-    }
-    else
-    {
-      motor.moveVelocity(-25);
-    }
-    if (sensor() < sensorPlace)
-    {
-      if (tilter() <= upPlace)
+      else
       {
-        tilter::motor.moveVelocity(-75);
+        motor.moveVelocity(75);
+        if (tilter() < upPlace)
+        {
+          tilter::motor.moveVelocity(75);
+        }
+      }
+      break;
+
+      case Controllers::DOWN:
+      if (sensor() > fastSpeedSpot)
+      {
+        motor.moveVelocity(-75);
+      }
+      else
+      {
+        motor.moveVelocity(-25);
+      }
+      if (sensor() < sensorPlace)
+      {
+        if (tilter() <= upPlace)
+        {
+          tilter::motor.moveVelocity(-75);
+        }
+      }
+      break;
+
+
+      case Controllers::DEINIT:
+      motor.moveVelocity(0);
+      tilter::motor.moveVelocity(0);
+      controller = Controllers::NONE;
+      break;
+
+      case Controllers::NONE:
+      break;
+    }
+  }
+
+  void init()
+  {
+    up();
+    down();
+    execute();
+  }
+  namespace auton
+  {
+    bool unpopped = true;
+    bool popped = false;
+    const double targetPosition = 25;
+    const double epsilon = 5;
+
+    void autonLiftUp(double targetPosition, double targetVelocity)
+    {
+      while (sensor() <= targetPosition)
+      {
+        motor.moveVelocity(targetVelocity);
+      }
+      if (sensor() >= targetPosition)
+      {
+        motor.moveVelocity(0);
       }
     }
-    break;
 
+    void autonLiftDown(double wantedPosition, double targetVelocity)
+    {
+      if (sensor() <= wantedPosition)
+      {
+        motor.moveVelocity(targetVelocity);
+      }
+      if (sensor() >= wantedPosition)
+      {
+        motor.moveVelocity(0);
+      }
+    }
 
-  case Controllers::DEINIT:
-    motor.moveVelocity(0);
-    tilter::motor.moveVelocity(0);
-    controller = Controllers::NONE;
-    break;
-
-  case Controllers::NONE:
-    break;
-  }
-}
-
-void init()
-{
-  up();
-  down();
-  execute();
-}
-namespace auton
-{
-bool unpopped = true;
-bool popped = false;
-const double targetPosition = 25;
-const double epsilon = 5;
-
-void autonLiftUp(double targetPosition, double targetVelocity)
-{
-  while (sensor() <= targetPosition)
-  {
-    motor.moveVelocity(targetVelocity);
-  }
-  if (sensor() >= targetPosition)
-  {
-    motor.moveVelocity(0);
-  }
-}
-
-void autonLiftDown(double wantedPosition, double targetVelocity)
-{
-  if (sensor() <= wantedPosition)
-  {
-    motor.moveVelocity(targetVelocity);
-  }
-  if (sensor() >= wantedPosition)
-  {
-    motor.moveVelocity(0);
-  }
-}
-
-void popOpen()
-{
-  if (sensor() > 2000)
-  {
-    motor.moveVelocity(90);
-    tilter::motor.moveVelocity(60);
-  }
-  if (sensor() <= 2000)
-  {
-    motor.moveVelocity(0);
-    tilter::motor.moveVelocity(0);
-  }
-}
-} // namespace auton
+    void popOpen()
+    {
+      if (sensor() > 2000)
+      {
+        motor.moveVelocity(90);
+        tilter::motor.moveVelocity(60);
+      }
+      if (sensor() <= 2000)
+      {
+        motor.moveVelocity(0);
+        tilter::motor.moveVelocity(0);
+      }
+    }
+  } // namespace auton
 } // namespace lift

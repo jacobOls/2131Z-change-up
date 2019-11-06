a31d86ccda8835bcbcd0a758aa0dad79372a2f06
Jacob <swordsparring@gmail.com>
2019-11-06 10:45:44 -0700

    formatting

--- Gen4/src/custom/auton/routines.cpp
+++ Gen4/src/custom/auton/routines.cpp
@@ -4,35 +4,38 @@
 #include "custom/systems/tilter.hpp"
 #include "custom/systems/intake.hpp"
 
-  namespace auton{
-    void wait(int time){
-      pros::delay(time);
-    }
+namespace auton
+{
+void wait(int time)
+{
+  pros::delay(time);
+}
 
-    void red5(){
-      intake::auton::intakeOn(200);
-      drive::auton::driving(800,60);
-        drive::auton::driving(-600,-200);
-        drive::auton::rightTurn(350,75);
-        drive::auton::driving(200,100);
-      intake::auton::intakeOn(-25);
-      wait(800);
-      tilter::auton::stack(400,25);
-      intake::auton::stackReverseAuton(-300,-300,-25);
-    }
+void red5()
+{
+  intake::auton::intakeOn(200);
+  drive::auton::driving(800, 60);
+  drive::auton::driving(-600, -200);
+  drive::auton::rightTurn(350, 75);
+  drive::auton::driving(200, 100);
+  intake::auton::intakeOn(-25);
+  wait(800);
+  tilter::auton::stack(400, 25);
+  intake::auton::stackReverseAuton(-300, -300, -25);
+}
 
-    void blue5(){
+void blue5()
+{
 
-      intake::auton::intakeOn(200);
-      drive::auton::driving(800,60);
-        drive::auton::driving(-600,-200);
-        drive::auton::leftTurn(350,75);
-        drive::auton::driving(200,100);
-      intake::auton::intakeOn(-25);
-      wait(800);
-      tilter::auton::stack(400,25);
-      intake::auton::stackReverseAuton(-300,-300,-25);
+  intake::auton::intakeOn(200);
+  drive::auton::driving(800, 60);
+  drive::auton::driving(-600, -200);
+  drive::auton::leftTurn(350, 75);
+  drive::auton::driving(200, 100);
+  intake::auton::intakeOn(-25);
+  wait(800);
+  tilter::auton::stack(400, 25);
+  intake::auton::stackReverseAuton(-300, -300, -25);
+}
 
-    }
-
-  }
+} // namespace auton
--- Gen4/src/custom/systems/tilter.cpp
+++ Gen4/src/custom/systems/tilter.cpp
@@ -6,154 +6,159 @@
 
 namespace tilter
 {
-  Controllers controller = Controllers::NONE;
-  double sensor()
+Controllers controller = Controllers::NONE;
+double sensor()
+{
+  return tilterSensor.get_value();
+}
+double lSensor()
+{
+  return lift::liftSensor.get_value();
+}
+
+int halfDown = 1850;
+int allDown = 1170;
+int upPlace = 1850;
+
+void up()
+{
+  if (BtnForward.isPressed())
   {
-    return tilterSensor.get_value();
+    controller = Controllers::FORWARD;
   }
-  double lSensor()
+  else if (controller == Controllers::FORWARD)
   {
-    return lift::liftSensor.get_value();
+    controller = Controllers::DEINIT;
   }
+}
 
-  int halfDown =1850;
-  int allDown = 1170;
-  int upPlace = 1850;
-
-  void up()
+void down()
+{
+  if (BtnBackward.isPressed())
   {
-    if (BtnForward.isPressed())
-    {
-      controller = Controllers::FORWARD;
-    }
-    else if (controller == Controllers::FORWARD)
-    {
-      controller = Controllers::DEINIT;
-    }
+    controller = Controllers::BACKWARD;
   }
-
-  void down()
+  else if (controller == Controllers::BACKWARD)
   {
-    if (BtnBackward.isPressed())
-    {
-      controller = Controllers::BACKWARD;
-    }
-    else if (controller == Controllers::BACKWARD)
-    {
-      controller = Controllers::DEINIT;
-    }
+    controller = Controllers::DEINIT;
   }
+}
 
-  void goUp(){
-    if(BtnGoUp.isPressed()){
-      controller = Controllers::TOUP;
-    }
-    else if(controller == Controllers::TOUP && motor.getActualVelocity() == 0){
-      controller = Controllers::DEINIT;
-    }
-  }
-
-
-
-  void returnDown()
+void goUp()
+{
+  if (BtnGoUp.isPressed())
   {
-    if (BtnReturn.isPressed())
-    {
-      controller = Controllers::RETURN;
-    }
-    else if (controller == Controllers::RETURN && motor.getActualVelocity() == 0)
-    {
-      controller = Controllers::DEINIT;
-    }
+    controller = Controllers::TOUP;
   }
-
-  void execute()
+  else if (controller == Controllers::TOUP && motor.getActualVelocity() == 0)
   {
-    if (controller == Controllers::BACKWARD && sensor() > 2150)
+    controller = Controllers::DEINIT;
+  }
+}
+
+void returnDown()
+{
+  if (BtnReturn.isPressed())
+  {
+    controller = Controllers::RETURN;
+  }
+  else if (controller == Controllers::RETURN && motor.getActualVelocity() == 0)
+  {
+    controller = Controllers::DEINIT;
+  }
+}
+
+void execute()
+{
+  if (controller == Controllers::BACKWARD && sensor() > 2150)
+  {
+    controller = Controllers::DEINIT;
+  }
+  if (controller == Controllers::FORWARD && sensor() <= 1460)
+  {
+    controller = Controllers::DEINIT;
+  }
+  switch (controller)
+  {
+
+  case Controllers::FORWARD:
+    motor.moveVelocity(75);
+    break;
+
+  case Controllers::BACKWARD:
+    motor.moveVelocity(-25);
+    break;
+
+  case Controllers::TOUP:
+    if (sensor() < upPlace)
     {
-      controller = Controllers::DEINIT;
+      motor.moveVelocity(-100);
     }
-    if (controller == Controllers::FORWARD && sensor() <= 1460)
+    else if (sensor() >= upPlace)
     {
-      controller = Controllers::DEINIT;
-    }
-    switch (controller)
-    {
-
-      case Controllers::FORWARD:
-      motor.moveVelocity(75);
-      break;
-
-      case Controllers::BACKWARD:
-      motor.moveVelocity(-25);
-      break;
-
-      case Controllers::TOUP:
-      if(sensor() < upPlace){
-        motor.moveVelocity(-100);
-      }
-      else if(sensor() >= upPlace)
-      { motor.moveVelocity(0);
-        controller = Controllers::DEINIT;
-      }
-      break;
-
-      case Controllers::RETURN:
-      if(lSensor() < 1250 && sensor() > 1520){
-        motor.moveVelocity(100);
-      }
-      else if(sensor() > 1870){
-        motor.moveVelocity(100);
-      }
-
-      break;
-
-      case Controllers::DEINIT:
       motor.moveVelocity(0);
-      controller = Controllers::NONE;
-      break;
-
-      case Controllers::NONE:
-      break;
+      controller = Controllers::DEINIT;
     }
-  }
+    break;
 
-  void init()
-  {
-    up();
-    down();
-    goUp();
-    execute();
-    returnDown();
-  }
-
-  namespace auton
-  {
-
-    void trayDown(double wantedPosition, double targetVelocity)
+  case Controllers::RETURN:
+    if (lSensor() < 1250 && sensor() > 1520)
     {
-      while (sensor() > wantedPosition)
-      {
-        motor.moveVelocity(targetVelocity);
-      }
-      if (motor.getPosition() <= wantedPosition)
-      {
-        motor.moveVelocity(0);
-      }
+      motor.moveVelocity(100);
     }
-
-    void stack(double wantedPosition, double targetVelocity)
+    else if (sensor() > 1870)
     {
-      while (sensor() < wantedPosition)
-      {
-        motor.moveVelocity(targetVelocity);
-      }
-      if (sensor() >= wantedPosition)
-      {
-        motor.moveVelocity(0);
-      }
+      motor.moveVelocity(100);
     }
 
-  } // namespace auton
+    break;
+
+  case Controllers::DEINIT:
+    motor.moveVelocity(0);
+    controller = Controllers::NONE;
+    break;
+
+  case Controllers::NONE:
+    break;
+  }
+}
+
+void init()
+{
+  up();
+  down();
+  goUp();
+  execute();
+  returnDown();
+}
+
+namespace auton
+{
+
+void trayDown(double wantedPosition, double targetVelocity)
+{
+  while (sensor() > wantedPosition)
+  {
+    motor.moveVelocity(targetVelocity);
+  }
+  if (motor.getPosition() <= wantedPosition)
+  {
+    motor.moveVelocity(0);
+  }
+}
+
+void stack(double wantedPosition, double targetVelocity)
+{
+  while (sensor() < wantedPosition)
+  {
+    motor.moveVelocity(targetVelocity);
+  }
+  if (sensor() >= wantedPosition)
+  {
+    motor.moveVelocity(0);
+  }
+}
+
+} // namespace auton
 
 } // namespace tilter

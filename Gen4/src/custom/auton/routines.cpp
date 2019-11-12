44bfebaded4ed5bd79f4a5bf506a3d11674d96be
Jacob <swordsparring@gmail.com>
2019-11-12 14:59:38 -0700

    skills auton draft

--- Gen4/src/custom/auton/routines.cpp
+++ Gen4/src/custom/auton/routines.cpp
@@ -173,4 +173,62 @@ namespace auton
     tilter::auton::trayDown(1500, 100);
   }
 
+  void skills(){
+    intake::auton::intakeOn(200);
+    drive::auton::driving(1100, 60);
+    drive::auton::leftTurn(15,50);
+    drive::auton::driving(100,50);
+    drive::auton::driveBack(-100,-50);
+    drive::auton::rightTurn(15,50);
+    drive::auton::driving(900,60);
+    drive::auton::rightTurn(250,50);
+    //stacking
+    intake::auton::intakeOn(-50);
+    wait(900);
+    intake::auton::intakeOff();
+    intake::auton::intakeOn(25);
+    wait(400);
+    intake::auton::intakeOff();
+    tilter::auton::stack(1800, 25);
+    intake::auton::intakeOn(-75);
+    tilter::auton::stack(1900, 25);
+    intake::auton::intakeOff();
+    tilter::auton::stack(2020, 25);
+    intake::auton::intakeOn(-200);
+    wait(250);
+    intake::auton::intakeOff();
+    intake::auton::stackReverseAuton(-100, 25, 25);
+    tilter::auton::trayDown(1500, 100);
+    intake::auton::stackReverseAuton(-100, 25, 25);
+    tilter::auton::trayDown(1500, 100);
+    //back to driving
+    intake::auton::intakeOn(200);
+    drive::auton::rightTurn(350,50);
+    drive::auton::driving(500,200);
+    drive::auton::leftTurn(200,50);
+    drive::auton::driving(1600,90);
+    drive::auton::leftTurn(200,50);
+    drive::auton::driving(1000,200);
+    //stack
+    intake::auton::intakeOn(-50);
+    wait(900);
+    intake::auton::intakeOff();
+    intake::auton::intakeOn(25);
+    wait(400);
+    intake::auton::intakeOff();
+    tilter::auton::stack(1800, 25);
+    intake::auton::intakeOn(-75);
+    tilter::auton::stack(1900, 25);
+    intake::auton::intakeOff();
+    tilter::auton::stack(2020, 25);
+    intake::auton::intakeOn(-200);
+    wait(250);
+    intake::auton::intakeOff();
+    intake::auton::stackReverseAuton(-100, 25, 25);
+    tilter::auton::trayDown(1500, 100);
+  }
+
+
+
+
 } // namespace auton

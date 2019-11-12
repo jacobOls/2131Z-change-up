ff2e58822aab358120c9a16ec112d7cd554201be
Jacob <swordsparring@gmail.com>
2019-11-12 14:43:17 -0700

    easier reading, 7 updates

--- Gen4/src/custom/auton/routines.cpp
+++ Gen4/src/custom/auton/routines.cpp
@@ -38,11 +38,11 @@ namespace auton
     drive::auton::driveBack(-300, -125);
     drive::auton::rightTurn(375, 50);
     drive::auton::driving(90, 100);
+    //stacking
     intake::auton::intakeOn(-50);
     wait(900);
     intake::auton::intakeOff();
     intake::auton::intakeOn(25);
-
     wait(400);
     intake::auton::intakeOff();
     tilter::auton::stack(1800, 25);
@@ -59,16 +59,16 @@ namespace auton
 
   void red7(){
     intake::auton::intakeOn(200);
-    drive::auton::driving(900, 80);
+    drive::auton::driving(600, 80);
     drive::auton::leftTurn(15,25);
-    drive::auton::driving(100,60);
-    drive::auton::driveBack(-150,-150);
-    drive::auton::rightTurn(40,50);
-    drive::auton::driving(400,60);
-    drive::auton::driveBack(-600,-200);
+    drive::auton::driveBack(-800,-200);
+    drive::auton::rightTurn(15,25);
+    drive::auton::driving(400,80);
+    drive::auton::driveBack(-300,-200);
     intake::auton::intakeOff();
-    drive::auton::rightTurn(300,75);
-    drive::auton::driving(300,150);
+    drive::auton::rightTurn(400,100);
+    drive::auton::driving(200,100);
+    //stacking
     intake::auton::intakeOn(-50);
     wait(900);
     intake::auton::intakeOff();
@@ -111,6 +111,7 @@ namespace auton
     drive::auton::driveBack(-300, -125);
     drive::auton::leftTurn(375, 50);
     drive::auton::driving(90, 100);
+    //stacking
     intake::auton::intakeOn(-50);
     wait(900);
     intake::auton::intakeOff();
@@ -122,8 +123,6 @@ namespace auton
     tilter::auton::stack(1900, 29);
     intake::auton::intakeOff();
     tilter::auton::stack(2050, 29);
-    // intake::auton::intakeOn(20);
-    // drive::auton::driving(25,25);
     intake::auton::intakeOn(-200);
     wait(250);
     intake::auton::intakeOff();
@@ -146,17 +145,16 @@ namespace auton
 
   void blue7(){
     intake::auton::intakeOn(200);
-    drive::auton::driving(800, 60);
-    drive::auton::driving(-150,-100);
-    drive::auton::rightTurn(5,25);
-    drive::auton::driving(150,60);
-    drive::auton::driving(-150,-100);
-    drive::auton::leftTurn(40,50);
-    drive::auton::driving(200,60);
-    drive::auton::driving(-600,-200);
+    drive::auton::driving(600, 80);
+    drive::auton::rightTurn(15,25);
+    drive::auton::driveBack(-800,-200);
+    drive::auton::leftTurn(15,25);
+    drive::auton::driving(400,80);
+    drive::auton::driveBack(-300,-200);
     intake::auton::intakeOff();
-    drive::auton::leftTurn(300,75);
-    drive::auton::driving(300,150);
+    drive::auton::leftTurn(400,100);
+    drive::auton::driving(200,100);
+    //stacking
     intake::auton::intakeOn(-50);
     wait(900);
     intake::auton::intakeOff();

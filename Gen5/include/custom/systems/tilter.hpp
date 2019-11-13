daf1be04f3296b9a5a2996074dae28d633b76ed1
Jacob <swordsparring@gmail.com>
2019-11-13 15:57:19 -0700

    slighly fancier tilter

--- Gen5/include/custom/systems/tilter.hpp
+++ Gen5/include/custom/systems/tilter.hpp
@@ -1,6 +1,16 @@
 #ifndef TILTER_HPP
 #define TILTER_HPP
 namespace tilter{
+  enum class Controllers
+{
+    FORWARD,
+    BACKWARD,
+    RETURN,
+    DEINIT,
+    TOUP,
+    NONE
+};
+void execute(void);
 void init(void);
 }
 
--- Gen5/src/custom/systems/tilter.cpp
+++ Gen5/src/custom/systems/tilter.cpp
@@ -5,6 +5,7 @@
 #include "custom/systems/lift.hpp"
 
 namespace tilter{
+  Controllers controller = Controllers::NONE;
   void tilterUp(){
     if(BtnForward.isPressed()){
       motor.moveVelocity(25);
@@ -22,6 +23,64 @@ namespace tilter{
       motor.moveVelocity(0);
     }
   }
+
+  void execute()
+    {
+      // if (controller == Controllers::FORWARD && sensor() > 2150)
+      // {
+      //   controller = Controllers::DEINIT;
+      // }
+      // if (controller == Controllers::BACKWARD && sensor() <= 1430)
+      // {
+      //   controller = Controllers::DEINIT;
+      // }
+      switch (controller)
+      {
+
+        case Controllers::FORWARD:
+        motor.moveVelocity(25);
+        // trayLock = false;
+        break;
+
+        case Controllers::BACKWARD:
+        motor.moveVelocity(-75);
+        // trayLock = false;
+        break;
+
+        case Controllers::TOUP:
+        // trayLock = true;
+        // if (sensor() < upPlace)
+        // {
+        //   motor.moveVelocity(100);
+        // }
+        // else if (sensor() >= upPlace)
+        // {
+        //   motor.moveVelocity(0);
+        //   controller = Controllers::DEINIT;
+        // }
+        break;
+
+        case Controllers::RETURN:
+        // if (lSensor() < 1250 && sensor() > 1520)
+        // {
+        //   motor.moveVelocity(-100);
+        // }
+        // else if (sensor() > 1870)
+        // {
+        //   motor.moveVelocity(-100);
+        // }
+
+        break;
+
+        case Controllers::DEINIT:
+        motor.moveVelocity(0);
+        controller = Controllers::NONE;
+        break;
+
+        case Controllers::NONE:
+        break;
+      }
+    }
 
   void init(){
     tilterUp();

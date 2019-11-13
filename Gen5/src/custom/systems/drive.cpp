50d5d60f699037096ad1f53d1fb32220dd493fe9
Jacob <swordsparring@gmail.com>
2019-11-13 16:16:26 -0700

    spacing so i dont die reading it

--- Gen5/src/custom/systems/drive.cpp
+++ Gen5/src/custom/systems/drive.cpp
@@ -11,15 +11,18 @@ namespace drive{
     if (std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) < 0.05 && std::abs(master.getAnalog(okapi::ControllerAnalog::leftX)) < 0.05){
       left_drive.moveVelocity(0);
     }
+
     else if(std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) > std::abs(master.getAnalog(okapi::ControllerAnalog::leftX))){
       left_drive.moveVelocity(master.getAnalog(okapi::ControllerAnalog::leftY)*200);
     }
+
     else if(std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) < std::abs(master.getAnalog(okapi::ControllerAnalog::leftX))){
       if(master.getAnalog(okapi::ControllerAnalog::leftX) > 0.05 && master.getAnalog(okapi::ControllerAnalog::leftX) > master.getAnalog(okapi::ControllerAnalog::rightX)){
         left_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::leftX)*200);
         right_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::leftX)*-200);
       }
     }
+
 
     if (std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) < 0.05 && std::abs(master.getAnalog(okapi::ControllerAnalog::rightX)) < 0.05){
       right_drive.moveVelocity(0);
@@ -28,6 +31,7 @@ namespace drive{
     else if(std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) > std::abs(master.getAnalog(okapi::ControllerAnalog::rightX))){
       right_drive.moveVelocity(master.getAnalog(okapi::ControllerAnalog::rightY)*200);
     }
+
     else if(std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) < std::abs(master.getAnalog(okapi::ControllerAnalog::rightX))){
       if(master.getAnalog(okapi::ControllerAnalog::rightX) > 0.05 && master.getAnalog(okapi::ControllerAnalog::rightX) > master.getAnalog(okapi::ControllerAnalog::leftX)){
         left_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::rightX)*200);
@@ -35,5 +39,4 @@ namespace drive{
       }
     }
   }
-
 }

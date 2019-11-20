7e8212defd259e42108825e62bca7538702f7e32
Jacob <swordsparring@gmail.com>
2019-11-20 09:27:32 -0700

    omni drive from mechnum drive

--- Gen5/src/custom/systems/drive.cpp
+++ Gen5/src/custom/systems/drive.cpp
@@ -2,41 +2,23 @@
 #include "custom/setup/motors.hpp"
 #include "custom/setup/controller.hpp"
 #include "custom/systems/drive.hpp"
-int leftXVel;
-int rightXVel;
-int leftYVel;
-int rightYVel;
+int leftVel;
+int rightVel;
+int lJoystickSpeed = 200;
+int rJoystickSpeed = 200;
 namespace drive{
   void drive(){
-    if (std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) < 0.05 && std::abs(master.getAnalog(okapi::ControllerAnalog::leftX)) < 0.05){
-      left_drive.moveVelocity(0);
-    }
+    if (std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) < 0.05)
+            leftVel = 0;
+        else
+            leftVel = (master.getAnalog(ControllerAnalog::leftY) * abs(lJoystickSpeed));
 
-    else if(std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) > std::abs(master.getAnalog(okapi::ControllerAnalog::leftX))){
-      left_drive.moveVelocity(master.getAnalog(okapi::ControllerAnalog::leftY)*200);
-    }
+        if (std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) < 0.05)
+            rightVel = 0;
+        else
+            rightVel = (master.getAnalog(ControllerAnalog::rightY) * abs(rJoystickSpeed));
 
-    else if(std::abs(master.getAnalog(okapi::ControllerAnalog::leftY)) < std::abs(master.getAnalog(okapi::ControllerAnalog::leftX))){
-      if(master.getAnalog(okapi::ControllerAnalog::leftX) < 0.05 && abs(master.getAnalog(okapi::ControllerAnalog::leftX)) > master.getAnalog(okapi::ControllerAnalog::rightX)){
-        left_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::leftX)*200);
-        right_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::leftX)*-200);
-      }
-    }
-
-
-    if (std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) < 0.05 && std::abs(master.getAnalog(okapi::ControllerAnalog::rightX)) < 0.05){
-      right_drive.moveVelocity(0);
-
-    }
-    else if(std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) > std::abs(master.getAnalog(okapi::ControllerAnalog::rightX))){
-      right_drive.moveVelocity(master.getAnalog(okapi::ControllerAnalog::rightY)*200);
-    }
-
-    else if(std::abs(master.getAnalog(okapi::ControllerAnalog::rightY)) < std::abs(master.getAnalog(okapi::ControllerAnalog::rightX))){
-      if(master.getAnalog(okapi::ControllerAnalog::rightX) > 0.05 && master.getAnalog(okapi::ControllerAnalog::rightX) > master.getAnalog(okapi::ControllerAnalog::leftX)){
-        left_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::rightX)*200);
-        right_strafe.moveVelocity(master.getAnalog(okapi::ControllerAnalog::rightX)*-200);
-      }
+        right_drive.moveVelocity(rightVel);
+        left_drive.moveVelocity(leftVel);
     }
   }
-}

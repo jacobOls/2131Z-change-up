302e5ca7af251b613aac82a204673a9c2abb30cf
Jacob <swordsparring@gmail.com>
2019-12-02 15:24:54 -0700

    intake and tilter can just be copied over, lol

--- Gen6/src/custom/systems/intake.cpp
+++ Gen6/src/custom/systems/intake.cpp
@@ -4,5 +4,147 @@
 #include "custom/systems/intake.hpp"
 
 namespace intake{
+  Controllers controller = Controllers::NONE;
+    int liftUp = 1300;
+    bool bothButtonsPressed(){
+      return BtnLeft.isPressed() && BtnRight.isPressed();
+    }
 
+    bool slowOut = false;
+    void intake(){
+
+      if (BtnIn.isPressed()){
+        controller = Controllers::IN;
+      }
+      else if (controller == Controllers::IN){
+        controller = Controllers::DEINIT;
+      }
+    }
+
+    void stackReverse(){
+      if (BtnBackUp.isPressed()){
+        drive::left_drive.moveVelocity(-25);
+        drive::right_drive.moveVelocity(-25);
+        controller = Controllers::MACRO;
+      }
+      else if (controller == Controllers::MACRO){
+        controller = Controllers::DEINIT;
+      }
+    }
+
+    void outake(){
+      if (BtnOut.isPressed() || bothButtonsPressed())
+      {
+        controller = Controllers::OUT;
+      }
+      else if (controller == Controllers::OUT)
+      {
+        controller = Controllers::DEINIT;
+      }
+    }
+
+    void spinR(){
+      if (BtnRight.isPressed()){
+        controller = Controllers::SPINR;
+      }
+      else if (controller == Controllers::SPINR){
+        controller = Controllers::DEINIT;
+      }
+    }
+
+    void spinL(){
+      if (BtnLeft.isPressed()){
+        controller = Controllers::SPINL;
+      }
+      else if (controller == Controllers::SPINL){
+        controller = Controllers::DEINIT;
+      }
+    }
+
+    void execute(){
+      switch (controller){
+
+        case Controllers::IN:
+        // if (lift::liftSensor.get_value() > liftUp)
+        // {
+        // intakegroup.moveVelocity(0);
+        // }
+        // else
+        // {
+        intakegroup.moveVelocity(200);
+        // }
+        break;
+
+        case Controllers::OUT:
+        // if (lift::liftSensor.get_value() > liftUp && cubeSensor.get_value() < 2700)
+        // {
+        //   slowOut = true;
+        // }
+        // if (slowOut)
+        // {
+        //   intakegroup.moveVelocity(-47);
+        // }
+        // else
+        // {
+        intakegroup.moveVelocity(-200);
+        // }
+        break;
+
+        case Controllers::SPINL:
+        left_motor.moveVelocity(50);
+        right_motor.moveVelocity(-50);
+        break;
+
+        case Controllers::SPINR:
+        left_motor.moveVelocity(-50);
+        right_motor.moveVelocity(50);
+        break;
+
+        case Controllers::MACRO:
+        intakegroup.moveVelocity(-35);
+        break;
+
+        case Controllers::DEINIT:
+        intakegroup.moveVelocity(0);
+        slowOut = false;
+        controller = Controllers::NONE;
+        break;
+
+        case Controllers::NONE:
+        break;
+      }
+    }
+
+    void init(){
+      intake();
+      outake();
+      spinR();
+      spinL();
+      execute();
+      stackReverse();
+    }
+    namespace auton{
+
+      void stackReverseAuton(double distance, double driveSpeed, double intakeSpeed){
+        while (drive::left_front.getPosition() > distance || drive::right_front.getPosition() > distance){
+          /* code */
+          drive::left_drive.moveVelocity(-driveSpeed);
+          drive::right_drive.moveVelocity(-driveSpeed);
+          intakegroup.moveVelocity(-intakeSpeed);
+        }
+        drive::left_drive.moveVelocity(0);
+        drive::right_drive.moveVelocity(0);
+        intakegroup.moveVelocity(0);
+        // drive::auton::resetPositions();
+      }
+
+      bool intakeRunning = false;
+      void intakeOn(double targetVelocity){
+        intakegroup.moveVelocity(targetVelocity);
+      }
+
+      void intakeOff(){
+        intakegroup.moveVelocity(0);
+      }
+    } // namespace auton
 }
--- Gen6/src/custom/systems/tilter.cpp
+++ Gen6/src/custom/systems/tilter.cpp
@@ -4,5 +4,107 @@
 #include "custom/systems/tilter.hpp"
 
 namespace tilter{
+  Controllers controller = Controllers::NONE;
+  void tilterUp(){
+    if(BtnForward.isPressed()){
+      controller = Controllers::FORWARD;
+    }
+    else if(controller == Controllers::FORWARD){
+      controller = Controllers::DEINIT;
+    }
+  }
 
+  void tilterDown(){
+    if(BtnBackward.isPressed()){
+      controller = Controllers::BACKWARD;
+    }
+    else if(controller == Controllers::BACKWARD){
+      controller = Controllers::DEINIT;
+    }
+  }
+
+  void execute()
+  {
+    // if (controller == Controllers::FORWARD && motor.getPosition() > 400)
+    // {
+    //   controller = Controllers::DEINIT;
+    // }
+    // if (controller == Controllers::BACKWARD && motor.getPosition() < 5)
+    // {
+    //   controller = Controllers::DEINIT;
+    // }
+    if(motor.getPosition() > 1684 && controller == Controllers::FORWARD){
+      controller = Controllers::DEINIT;
+    }
+    if(motor.getPosition() < 5 && controller == Controllers::BACKWARD){
+      controller = Controllers::DEINIT;
+    }
+    switch (controller)
+    {
+      case Controllers::FORWARD:
+      motor.moveVelocity(50);
+      // trayLock = false;
+      break;
+
+      case Controllers::BACKWARD:
+      motor.moveVelocity(-100);
+      intake::intakegroup.moveVelocity(-28);
+      // trayLock = false;
+      break;
+
+      case Controllers::TOUP:
+      // trayLock = true;
+      if(motor.getPosition() < 250){
+        motor.moveVelocity(50);
+      }
+      // if (sensor() < upPlace)
+      // {
+      //   motor.moveVelocity(100);
+      // }
+      // else if (sensor() >= upPlace)
+      // {
+      //   motor.moveVelocity(0);
+      //   controller = Controllers::DEINIT;
+      // }
+      break;
+
+      case Controllers::RETURN:
+      if (lift::motor.getPosition() < 1250 && motor.getPosition() > 1520)
+      {
+        motor.moveVelocity(-100);
+      }
+      else if (motor.getPosition() > 1870)
+      {
+        motor.moveVelocity(-100);
+      }
+
+
+      break;
+
+      case Controllers::DEINIT:
+      motor.moveVelocity(0);
+      controller = Controllers::NONE;
+      break;
+
+      case Controllers::NONE:
+      break;
+    }
+  }
+
+  void init(){
+    tilterUp();
+    tilterDown();
+    execute();
+  }
+
+  namespace auton{
+
+    void tilter(int pos, int velocity){
+      if(motor.getPosition() > pos){
+        velocity = -velocity;
+      }
+      motor.moveAbsolute(pos,velocity);
+
+    }
+  }
 }

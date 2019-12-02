4d68894146683610b1a7ee38ce111a3817b025f2
Jacob <swordsparring@gmail.com>
2019-12-02 15:36:23 -0700

    more setup

---
+++ Gen6/include/custom/systems/lift.hpp
@@ -0,0 +1,20 @@
+#ifndef LIFT_HPP
+#define LIFT_HPP
+namespace lift{
+
+  enum class Controllers{
+    UP,
+    DOWN,
+    DEINIT,
+    NONE
+  };
+  void init(void);
+  void execute(void);
+  void init(void);
+  namespace auton{
+    void lift(double pos, double velocity);
+  }
+}
+
+
+#endif /* end of include guard: LIFT_HPP */
--- Gen6/src/custom/systems/lift.cpp
+++ Gen6/src/custom/systems/lift.cpp
@@ -4,5 +4,64 @@
 #include "custom/systems/lift.hpp"
 
 namespace lift{
+  Controllers controller = Controllers::NONE;
+  void up(){
+    if(BtnUp.isPressed()){
+      controller = Controllers::UP;
+    }
+    else if(controller == Controllers::UP){
+      controller = Controllers::DEINIT;
+    }
+  }
 
+  void down(){
+    if(BtnDown.isPressed()){
+      controller = Controllers::DOWN;
+    }
+    else if(controller == Controllers::DOWN){
+      controller = Controllers::DEINIT;
+    }
+  }
+
+
+  void execute(){
+
+    switch (controller)
+    {
+
+      case Controllers::UP:
+
+      break;
+
+      case Controllers::DOWN:
+
+
+      break;
+      case Controllers::DEINIT:
+      motor.moveVelocity(0);
+      controller = Controllers::NONE;
+      break;
+
+      case Controllers::NONE:
+      break;
+    }
+  }
+
+  void init(){
+    up();
+    down();
+    execute();
+  }
+
+  namespace auton{
+
+    void lift(int pos, int velocity){
+      if(motor.getPosition() > pos){
+        velocity = -velocity;
+      }
+      motor.moveAbsolute(pos,velocity);
+    }
+
+
+  }
 }

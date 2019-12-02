0ea21a73d89e0515b60b4639984fd35d5f33a316
Jacob <swordsparring@gmail.com>
2019-12-02 15:15:06 -0700

    Update drive.cpp

--- Gen6/src/custom/systems/drive.cpp
+++ Gen6/src/custom/systems/drive.cpp
@@ -11,11 +11,41 @@ namespace drive{
   int rStick(){
     return std::abs(master.getAnalog(okapi::ControllerAnalog::rightY));
   }
-void userDrive(){
+  int height(){
+    return std::abs(lift::motor.getPosition()/500);
+  }
+  void userDrive(){
+    if(lift::motor.getPosition() < 300){
+      if(lStick() < 0.5){
+        left_drive.moveVelocity(0);
+      }
+      else{
+        left_drive.moveVelocity(lStick()*200);
+      }
 
 
+      if(rStick() < 0.5){
+        right_drive.moveVelocity(0);
+      }
+      else{
+        right_drive.moveVelocity(rStick()*200);
+      }
+    }
+    else{
+      if(lStick() < 0.5){
+        left_drive.moveVelocity(0);
+      }
+      else{
+        left_drive.moveVelocity(lStick()*200/height());
+      }
 
 
-}
-
+      if(rStick() < 0.5){
+        right_drive.moveVelocity(0);
+      }
+      else{
+        right_drive.moveVelocity(rStick()*200/height());
+      }
+    }
+  }
 }

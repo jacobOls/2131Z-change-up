870bf2fe19eac0e2537cb2cc6324088c203ceacc
Jacob <swordsparring@gmail.com>
2019-12-04 14:50:10 -0700

    looks nicer

--- Gen5/src/main.cpp
+++ Gen5/src/main.cpp
@@ -7,10 +7,7 @@
 #include "custom/auton/selection.hpp"
 #include "custom/auton.hpp"
 /**
-* A callback function for LLEMU's center button.
-*
-* When this callback is fired, it will toggle line 2 of the LCD text between
-* "I was pressed!" and nothing.
+
 */
 
 /**

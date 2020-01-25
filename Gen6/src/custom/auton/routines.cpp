2a05d547274bed565f0c9bd22fe9a1d87fff52bc
Jacob <swordsparring@gmail.com>
2020-01-25 08:32:30 -0700

    blue auton

--- Gen6/src/custom/auton/routines.cpp
+++ Gen6/src/custom/auton/routines.cpp
@@ -95,26 +95,24 @@ namespace auton{
     startup();
     drive::auton::drive(-30,-125);
     drive::auton::drive(50,100);
-    drive::auton::turn(37,75);
+    drive::auton::turn(40,75);
     // lift::motor.moveAbsolute(150,200);
-    drive::auton::driveNRE(80,100);
-    wait(100);
+    drive::auton::driveNRE(75,100);
+    wait(200);
     lift::motor.moveAbsolute(2650,200);
-    wait(150);
-    drive::auton::driveNRB(85,200);
-    drive::auton::turn(36,-60);
+    drive::auton::driveNRB(90,200);
+    drive::auton::turn(49,-70);
     // intake::auton::intakeOff();
-    drive::auton::drive(82,180);
+    drive::auton::drive(82,150);
     intake::auton::intakeOn(200);
     // wait(300);
     // drive::auton::drive(35,200);
     lift::auton::lift(300,160,startTime,9500);
-    drive::auton::drive(-20,-100);
-    drive::auton::turn(260,100);
+    drive::auton::turn(270,120);
     lift::motor.moveAbsolute(0,200);
     drive::auton::driveNRE(300,200);
     tilter::motor.moveAbsolute(350,50);
-    drive::auton::driveNRB(75,200);
+    drive::auton::driveNRB(25,200);
     intake::auton::intakeOff();
     //stack
     tilter::auton::tilter(650,95);
@@ -125,8 +123,49 @@ namespace auton{
     intake::auton::intakeOn(-15);
     drive::auton::drive(-200,-85);
     tilter::motor.moveAbsolute(0,-200);
+    lift::auton::lift(2700,200,14151,1235);
   }
 
+
+
+
+
+/*
+const int startTime = pros::millis();
+startup();
+drive::auton::drive(-30,-125);
+drive::auton::drive(50,100);
+drive::auton::turn(37,75);
+// lift::motor.moveAbsolute(150,200);
+drive::auton::driveNRE(80,100);
+wait(100);
+lift::motor.moveAbsolute(2650,200);
+wait(150);
+drive::auton::driveNRB(85,200);
+drive::auton::turn(42,-60);
+// intake::auton::intakeOff();
+drive::auton::drive(82,180);
+intake::auton::intakeOn(200);
+// wait(300);
+// drive::auton::drive(35,200);
+lift::auton::lift(300,160,startTime,9500);
+drive::auton::drive(-20,-100);
+drive::auton::turn(260,100);
+lift::motor.moveAbsolute(0,200);
+drive::auton::driveNRE(300,200);
+tilter::motor.moveAbsolute(350,50);
+drive::auton::driveNRB(75,200);
+intake::auton::intakeOff();
+//stack
+tilter::auton::tilter(650,95);
+intake::auton::intakeOn(-45);
+tilter::auton::tilter(850,75);
+intake::auton::intakeOff();
+tilter::auton::tilter(1050,50);
+intake::auton::intakeOn(-15);
+drive::auton::drive(-200,-85);
+tilter::motor.moveAbsolute(0,-200);
+*/
   void blueSmall(){
     startup();
     drive::auton::drive(1100,70);

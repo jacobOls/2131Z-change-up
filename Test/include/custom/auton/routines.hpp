1643ca8e1d36362f611028908fb9a2eda681eb85
Jacob <swordsparring@gmail.com>
2020-01-27 15:48:43 -0700

    test

---
+++ Test/include/custom/auton/routines.hpp
@@ -0,0 +1,17 @@
+#ifndef ROUTINES_HPP
+#define ROUTINES_HPP
+
+namespace auton{
+  void redBig();
+  void redSmall();
+  void blueBig();
+  void blueSmall();
+  void doNothing();
+  void driveABit();
+  void nothing();
+  void onePoint();
+  void safeBlue();
+  void safeRed();
+}
+
+#endif /* end of include guard: ROUTINES_HPP */
---
+++ Test/src/custom/systems/lift.cpp
@@ -0,0 +1,87 @@
+#include "main.h"
+#include "custom/setup/motors.hpp"
+#include "custom/setup/controller.hpp"
+#include "custom/systems/lift.hpp"
+
+namespace lift{
+  Controllers controller = Controllers::NONE;
+  void up(){
+    if(BtnUp.isPressed()){
+      controller = Controllers::UP;
+    }
+    else if(controller == Controllers::UP){
+      controller = Controllers::DEINIT;
+    }
+  }
+
+  void down(){
+    if(BtnDown.isPressed()){
+      controller = Controllers::DOWN;
+    }
+    else if(controller == Controllers::DOWN){
+      controller = Controllers::DEINIT;
+    }
+  }
+
+  void pulse(){
+    motor.moveVelocity(-125);
+    pros::delay(300);
+    motor.moveVelocity(125);
+    pros::delay(300);
+    motor.moveVelocity(0);
+  }
+
+  void execute(){
+    if(motor.getPosition() < 5 && controller == Controllers::DOWN){
+      controller = Controllers::DEINIT;
+    }
+    switch (controller)
+    {
+
+      case Controllers::UP:
+      motor.moveVelocity(200);
+      break;
+
+      case Controllers::DOWN:
+      motor.moveVelocity(-200);
+
+      break;
+      case Controllers::DEINIT:
+      motor.moveVelocity(0);
+      controller = Controllers::NONE;
+      break;
+
+      case Controllers::NONE:
+      break;
+    }
+  }
+
+  void init(){
+    up();
+    down();
+    execute();
+  }
+
+  namespace auton{
+
+
+
+    void lift(int pos, int velocity,int startTime, double stopTime){
+
+      if(motor.getPosition() > pos && velocity > 1){
+        velocity = -velocity;
+        while(motor.getPosition() > pos && pros::millis() - startTime < stopTime){
+          motor.moveAbsolute(pos,velocity);
+
+        }
+      }
+      else{
+        while(motor.getPosition() < pos && pros::millis() - startTime < stopTime){
+          motor.moveAbsolute(pos,velocity);
+
+        }
+      }
+
+    }
+  }
+}

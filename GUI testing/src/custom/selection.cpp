53cbcd8660982c848106fcf284f974bc98e40c69
Jacob <swordsparring@gmail.com>
2020-01-30 13:41:10 -0700

    got stuff to try

--- GUI testing/src/custom/selection.cpp
+++ GUI testing/src/custom/selection.cpp
@@ -12,8 +12,8 @@ namespace selection{
   {"LR ","SR ","LB" ,"SB "},
   {"LR ","SR ","LB" ,"SB "}
 };
-lv_style_t myButtonStyleREL; //relesed style
-lv_style_t myButtonStylePR; //pressed style
+lv_style_t relStyle; //relesed style
+lv_style_t preStyle; //pressed style
 
 
 void isPressed(_lv_obj_t *pressedBtn, _lv_obj_t *Btn){
@@ -29,7 +29,7 @@ void isPressed(_lv_obj_t *pressedBtn, _lv_obj_t *Btn){
 
 
 static lv_res_t checkPress(_lv_obj_t *pressedBtn){
-  // isPressed(pressedBtn,btn1);
+  // isPressed(pressedBtn, btn1);
   std::cout << "running checkPress" <<  std::endl;
   lv_btn_set_state(pressedBtn, LV_BTN_STATE_TGL_PR); //fa
   return LV_RES_OK;
@@ -45,15 +45,42 @@ void btnLocNSize(lv_obj_t*btn,  int row, int col){
   lv_btn_set_toggle(btn, true);
   label = lv_label_create(btn, NULL);
   lv_label_set_text(label, btns[row][col].c_str());
-  lv_btn_set_style(btn, LV_BTN_STYLE_REL, &myButtonStyleREL); //set the relesed style
-   lv_btn_set_style(btn, LV_BTN_STYLE_PR, &myButtonStylePR); //set the pressed style
+  lv_btn_set_style(btn, LV_BTN_STYLE_REL, &relStyle); //set the relesed style
+   lv_btn_set_style(btn, LV_BTN_STYLE_PR, &preStyle); //set the pressed style
   lv_btn_set_action(btn, LV_BTN_ACTION_CLICK, checkPress);
   // lv_btn_set_state(btn, LV_BTN_STATE_REL);
   std::cout << "creating button " << btns[row][col].c_str() << std::endl;
 }
 
 
+void btnToggled(lv_obj_t * btn, bool toggled)
+{
+    if(toggled != (lv_btn_get_state(btn) >= 2)) lv_btn_toggle(btn);
+}
 
+lv_obj_t *btn1 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn2 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn3 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn4 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn5 = lv_btn_create(lv_scr_act(), NULL);
+
+lv_obj_t *btn6 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn7 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn8 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn9 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn10 = lv_btn_create(lv_scr_act(), NULL);
+
+lv_obj_t *btn11 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn12 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn13 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn14 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn15 = lv_btn_create(lv_scr_act(), NULL);
+
+lv_obj_t *btn16 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn17 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn18 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn19 = lv_btn_create(lv_scr_act(), NULL);
+lv_obj_t *btn20 = lv_btn_create(lv_scr_act(), NULL);
 
 void btnCreate(){
   // for (int row = 0; row < 5; row++) {
@@ -64,41 +91,16 @@ void btnCreate(){
   //   }
   //
   // }
-  lv_style_copy(&myButtonStyleREL, &lv_style_plain);
-   myButtonStyleREL.body.main_color = LV_COLOR_MAKE(150, 0, 0);
-   myButtonStyleREL.body.grad_color = LV_COLOR_MAKE(0, 0, 150);
-   myButtonStyleREL.body.radius = 0;
-   myButtonStyleREL.text.color = LV_COLOR_MAKE(255, 255, 255);
+  lv_style_copy(&relStyle, &lv_style_plain);
+   relStyle.body.main_color = LV_COLOR_MAKE(150, 0, 0);
+   relStyle.body.radius = 0;
+   relStyle.text.color = LV_COLOR_MAKE(255, 255, 255);
 
-   lv_style_copy(&myButtonStylePR, &lv_style_plain);
-   myButtonStylePR.body.main_color = LV_COLOR_MAKE(255, 0, 0);
-   myButtonStylePR.body.grad_color = LV_COLOR_MAKE(0, 0, 255);
-   myButtonStylePR.body.radius = 0;
-   myButtonStylePR.text.color = LV_COLOR_MAKE(255, 255, 255);
+   lv_style_copy(&preStyle, &lv_style_plain);
+   preStyle.body.main_color = LV_COLOR_MAKE(255, 0, 0);
+   preStyle.body.radius = 0;
+   preStyle.text.color = LV_COLOR_MAKE(255, 255, 255);
 
-  lv_obj_t *btn1 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn2 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn3 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn4 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn5 = lv_btn_create(lv_scr_act(), NULL);
-
-  lv_obj_t *btn6 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn7 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn8 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn9 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn10 = lv_btn_create(lv_scr_act(), NULL);
-
-  lv_obj_t *btn11 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn12 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn13 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn14 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn15 = lv_btn_create(lv_scr_act(), NULL);
-
-  lv_obj_t *btn16 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn17 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn18 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn19 = lv_btn_create(lv_scr_act(), NULL);
-  lv_obj_t *btn20 = lv_btn_create(lv_scr_act(), NULL);
 
   btnLocNSize(btn1,0,0);
   btnLocNSize(btn2,1,0);
@@ -127,21 +129,31 @@ void btnCreate(){
 }
 
 
+void btnChecker(){
+  btnToggled(btn1, true);
+  btnToggled(btn2, true);
+  btnToggled(btn3, true);
+  btnToggled(btn4, true);
+  btnToggled(btn5, true);
+
+}
 
 
-// void guiTask(void *param) {
-//   while(true) {
-//     // std::uint32_t start = pros::millis();
-//     // pros::Task::delay_until(&start, 10);
-//     pros::delay(10);
-//     // std::cout << "loop iteration" << std::endl;
-//   }
-// }
+
+void guiTask(void *param) {
+  while(true) {
+    // std::uint32_t start = pros::millis();
+    // pros::Task::delay_until(&start, 10);
+    btnChecker();
+    pros::delay(10);
+    // std::cout << "loop iteration" << std::endl;
+  }
+}
 
 void init(){
   btnCreate();
-  // std::cout << "starting task" << std::endl;
-  // pros::Task gui_task(guiTask, (void*)"some param", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "task name");
+  std::cout << "starting task" << std::endl;
+  pros::Task gui_task(guiTask, (void*)"some param", TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT, "task name");
 
   // gui_task.resume();
 }
@@ -159,33 +171,3 @@ LV_BTN_STYLE_INA - style of the inactive state. Default: lv_style_btn_ina.
 
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-// static lv_disp_drv_t disp_dr;
-// static lv_color_t buf_1[480 * 10];
-//
-// lv_obj_t *par = lv_btn_create(lv_scr_act(), NULL);
-// static lv_res_t OnClickPos(_lv_obj_t *pressedBtn);
-//
-// void btnInit(lv_obj_t *btn){
-// lv_obj_set_size(btn, 100, 50);	 /*Button size*/
-// lv_obj_set_pos(btn, 100,100);
-// }
-//
-// void selectionInit(){
-// btnInit(par);
-//
-//
-//
-// }

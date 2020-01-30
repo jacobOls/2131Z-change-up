c4e9e9a6b2ac171e539f9e6993edfb7c91f803f0
Jacob <swordsparring@gmail.com>
2020-01-30 15:37:41 -0700

    Update selection.cpp

--- GUI testing/src/custom/selection.cpp
+++ GUI testing/src/custom/selection.cpp
@@ -5,7 +5,34 @@
 #include <algorithm>
 #include <string>
 
+
 namespace selection{
+
+  lv_obj_t *btn1 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn2 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn3 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn4 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn5 = lv_btn_create(lv_scr_act(), NULL);
+
+  lv_obj_t *btn6 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn7 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn8 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn9 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn10 = lv_btn_create(lv_scr_act(), NULL);
+
+  lv_obj_t *btn11 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn12 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn13 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn14 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn15 = lv_btn_create(lv_scr_act(), NULL);
+
+  lv_obj_t *btn16 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn17 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn18 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn19 = lv_btn_create(lv_scr_act(), NULL);
+  lv_obj_t *btn20 = lv_btn_create(lv_scr_act(), NULL);
+
+
   std::string btns[5][4] ={ {"LR ","SR ","LB ","SB "},
   {"LR ","SR ","LB ","SB "},
   {"LR ","SR ","LB" ,"SB "},
@@ -13,31 +40,22 @@ namespace selection{
   {"LR ","SR ","LB" ,"SB "}
 };
 lv_style_t relStyle; //relesed style
-lv_style_t preStyle; //pressed style
+lv_style_t togStyle; //toggled  style
 
 
-void isPressed(_lv_obj_t *pressedBtn, _lv_obj_t *Btn){
-  if (pressedBtn == Btn)
-  {
 
-  }
-  else{
-    lv_btn_set_state(Btn, LV_BTN_STATE_REL);
-  }
+
+void btnToggled(lv_obj_t * btn, bool toggled)
+{
+  if(toggled != (lv_btn_get_state(btn) >= 2)) lv_btn_toggle(btn);
 }
-
-
 
 static lv_res_t btn_click_action(lv_obj_t * btn)
 {
-    uint8_t id = lv_obj_get_free_num(btn); //id usefull when there are multiple buttons
+  std::cout << "button action" << std::endl;
+  btnToggled(btn1, true);
 
-    if(id == 0)
-    {
         // char buffer[100];
-
-	 std::cout << "loop iteration" << std::endl;
-    }
 
     return LV_RES_OK;
 }
@@ -49,45 +67,17 @@ void btnConfig(lv_obj_t*btn,  int row, int col){
   int y = 240 / 5 * row;
   lv_obj_set_pos(btn, x,y );
   lv_obj_set_size(btn, 465 / 4, 240 / 5);
-  // lv_btn_set_toggle(btn, true);
+  lv_btn_set_toggle(btn, true);
   label = lv_label_create(btn, NULL);
   lv_label_set_text(label, btns[row][col].c_str());
 lv_btn_set_style(btn, LV_BTN_STYLE_REL, &relStyle); //set the relesed style
-lv_btn_set_style(btn, LV_BTN_STYLE_PR, &preStyle);
-lv_btn_set_action(btn, LV_BTN_ACTION_CLICK, btn_click_action); //set function to be called on button click
-  // lv_btn_set_action(btn, LV_BTN_ACTION_CLICK, checkPress);
-  // lv_btn_set_state(btn, LV_BTN_STATE_REL);
-  std::cout << "creating button " << btns[row][col].c_str() << std::endl;
+lv_btn_set_style(btn, LV_BTN_STYLE_PR, &togStyle); //set the pressed style
+lv_btn_set_action(btn, LV_BTN_ACTION_CLICK, btn_click_action);
+  lv_btn_set_state(btn, LV_BTN_STATE_REL);
+  // std::cout << "creating button " << btns[row][col].c_str() << std::endl;
 }
 
-void btnToggled(lv_obj_t * btn, bool toggled)
-{
-    if(toggled != (lv_btn_get_state(btn) )) lv_btn_toggle(btn);
-}
 
-lv_obj_t *btn1 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn2 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn3 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn4 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn5 = lv_btn_create(lv_scr_act(), NULL);
-
-lv_obj_t *btn6 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn7 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn8 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn9 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn10 = lv_btn_create(lv_scr_act(), NULL);
-
-lv_obj_t *btn11 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn12 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn13 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn14 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn15 = lv_btn_create(lv_scr_act(), NULL);
-
-lv_obj_t *btn16 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn17 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn18 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn19 = lv_btn_create(lv_scr_act(), NULL);
-lv_obj_t *btn20 = lv_btn_create(lv_scr_act(), NULL);
 
 
 void btnCreate(){
@@ -104,31 +94,31 @@ void btnCreate(){
    relStyle.body.radius = 3;
    relStyle.text.color = LV_COLOR_MAKE(150, 5,125);
 
-   lv_style_copy(&preStyle, &lv_style_plain);
-   preStyle.body.main_color = LV_COLOR_MAKE(255, 0, 0);
-   preStyle.body.radius = 3;
-   preStyle.text.color = LV_COLOR_MAKE(0, 164, 255);
+   lv_style_copy(&togStyle, &lv_style_plain);
+   togStyle.body.main_color = LV_COLOR_MAKE(255, 0, 0);
+   togStyle.body.radius = 3;
+   togStyle.text.color = LV_COLOR_MAKE(0, 164, 255);
 
 
-  btnConfig(btn1,0,0);
+  btnConfig(btn1,0,0); //collumn 1
   btnConfig(btn2,1,0);
   btnConfig(btn3,2,0);
   btnConfig(btn4,3,0);
   btnConfig(btn5,4,0);
 
-  btnConfig(btn6,0,1);
+  btnConfig(btn6,0,1); //collumn 2
   btnConfig(btn7,1,1);
   btnConfig(btn8,2,1);
   btnConfig(btn9,3,1);
   btnConfig(btn10,4,1);
 
-  btnConfig(btn11,0,2);
+  btnConfig(btn11,0,2); //collumn 3
   btnConfig(btn12,1,2);
   btnConfig(btn13,2,2);
   btnConfig(btn14,3,2);
   btnConfig(btn15,4,2);
 
-  btnConfig(btn16,0,3);
+  btnConfig(btn16,0,3); //collumn 4
   btnConfig(btn17,1,3);
   btnConfig(btn18,2,3);
   btnConfig(btn19,3,3);
@@ -138,18 +128,19 @@ void btnCreate(){
 
 
 void btnChecker(){
-  // pros::delay(20);
+  pros::delay(20);
   btnToggled(btn1, true);
-  btnToggled(btn2, false);
-  btnToggled(btn3, false);
+  btnToggled(btn2, true);
+  btnToggled(btn3, true);
   btnToggled(btn4, false);
   btnToggled(btn5, false);
-std::cout << "yeet" << std::endl;
+// std::cout << "yeet" << std::endl;
 }
 
 
 void clickBtn(){
 // std::cout << "running" << std::endl;
+
 
 }
 

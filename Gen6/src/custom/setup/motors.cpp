d6e08754610ecb3967b73cdf8cc22668b75f0cd7
Jacob <swordsparring@gmail.com>
2020-01-30 15:04:59 -0700

    prot changes

--- Gen6/src/custom/setup/motors.cpp
+++ Gen6/src/custom/setup/motors.cpp
@@ -2,13 +2,13 @@
 #include "custom/setup/motors.hpp"
 #include "custom/setup/ramping.hpp"
 namespace drive{
-  okapi::Motor left_back(12, false, okapi::AbstractMotor::gearset::green,
+  okapi::Motor left_back(14, false, okapi::AbstractMotor::gearset::green,
     okapi::AbstractMotor::encoderUnits::degrees);
     okapi::Motor left_front(11, true, okapi::AbstractMotor::gearset::green,
       okapi::AbstractMotor::encoderUnits::degrees);
-      okapi::Motor right_back(13, true, okapi::AbstractMotor::gearset::green,
+      okapi::Motor right_back(12, true, okapi::AbstractMotor::gearset::green,
         okapi::AbstractMotor::encoderUnits::degrees);
-        okapi::Motor right_front(14, false, okapi::AbstractMotor::gearset::green,
+        okapi::Motor right_front(13, false, okapi::AbstractMotor::gearset::green,
           okapi::AbstractMotor::encoderUnits::degrees);
           okapi::MotorGroup left_drive({left_front, left_back});
           okapi::MotorGroup right_drive({right_front, right_back});
@@ -21,9 +21,9 @@ namespace drive{
         }
 //16 and 18 burned
         namespace intake{
-          okapi::Motor left_motor(21, false, okapi::AbstractMotor::gearset::green, //left
+          okapi::Motor left_motor(2, false, okapi::AbstractMotor::gearset::green, //left
             okapi::AbstractMotor::encoderUnits::degrees);
-            okapi::Motor right_motor(20, true, okapi::AbstractMotor::gearset::green, //right
+            okapi::Motor right_motor(1, true, okapi::AbstractMotor::gearset::green, //right
               okapi::AbstractMotor::encoderUnits::degrees);
 
               okapi::MotorGroup intakegroup({left_motor, right_motor});
@@ -32,14 +32,14 @@ namespace drive{
             } // namespace intake
 
             namespace tilter{
-              okapi::Motor motor(9, false, okapi::AbstractMotor::gearset::red,
+              okapi::Motor motor(4, false, okapi::AbstractMotor::gearset::red,
                 okapi::AbstractMotor::encoderUnits::degrees);
                 // pros::ADIAnalogIn tilterSensor(3);
 
               } // namespace tilter
 
               namespace lift{
-                okapi::Motor motor(17, true, okapi::AbstractMotor::gearset::red,
+                okapi::Motor motor(3, true, okapi::AbstractMotor::gearset::red,
                   okapi::AbstractMotor::encoderUnits::degrees);
                   // pros::ADIAnalogIn liftSensor(1);
 

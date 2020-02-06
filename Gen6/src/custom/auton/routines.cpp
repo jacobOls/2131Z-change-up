de11acad311bd91498361caf1cd4feda189fc650
Jacob <swordsparring@gmail.com>
2020-02-06 08:36:20 -0700

    auto formatting

--- Gen6/src/custom/auton/routines.cpp
+++ Gen6/src/custom/auton/routines.cpp
@@ -4,107 +4,110 @@
 #include "custom/systems/tilter.hpp"
 #include "custom/systems/intake.hpp"
 #include "custom/setup/motors.hpp"
-namespace auton{
+namespace auton
+{
 
 //managment stuff
 
-  void wait(int time){
-    pros::delay(time);
-  }
-  //positive turn is left
-  //negative turn is right
-  void startup(){
-    lift::motor.tarePosition();
-    tilter::motor.tarePosition();
-    drive::auton::resetPos();
-    // lift::auton::lift(1000,200);
-    intake::auton::intakeOn(-200);
-    lift::pulse();
-    // wait(500);
-    intake::auton::intakeOn(200);
-    wait(800);
-    // lift::auton::lift(0,200);
-  }
-
+void wait(int time)
+{
+  pros::delay(time);
+}
+//positive turn is left
+//negative turn is right
+void startup()
+{
+  lift::motor.tarePosition();
+  tilter::motor.tarePosition();
+  drive::auton::resetPos();
+  // lift::auton::lift(1000,200);
+  intake::auton::intakeOn(-200);
+  lift::pulse();
+  // wait(500);
+  intake::auton::intakeOn(200);
+  wait(800);
+  // lift::auton::lift(0,200);
+}
 
 //red autons
 
-  void redBig(){
-    const int startTime = pros::millis();
-    startup();
-    drive::auton::drive(-30,-125);
-    drive::auton::drive(50,100);
-    drive::auton::turn(40,-75);
-    // lift::motor.moveAbsolute(150,200);
-    drive::auton::driveNRE(65,100);
-    wait(200);
-    lift::motor.moveAbsolute(2650,200);
-    drive::auton::driveNRB(100,200);
-    drive::auton::turn(48,70);
-    // intake::auton::intakeOff();
-    drive::auton::drive(90,170);
-    intake::auton::intakeOn(200);
-    // wait(300);
-    // drive::auton::drive(35,200);
-    lift::auton::lift(300,160,startTime,9500);
-    drive::auton::turn(260,-120);
-    lift::motor.moveAbsolute(0,200);
-    drive::auton::driveNRE(300,200);
-    tilter::motor.moveAbsolute(350,50);
-    drive::auton::driveNRB(25,200);
-    intake::auton::intakeOff();
-    //stack
-    tilter::auton::tilter(650,95);
-    intake::auton::intakeOn(-45);
-    tilter::auton::tilter(850,75);
-    intake::auton::intakeOff();
-    tilter::auton::tilter(1050,50);
-    intake::auton::intakeOn(-15);
-    drive::auton::drive(-200,-85);
-    tilter::motor.moveAbsolute(0,-200);
-    lift::auton::lift(2700,200,14151,1235);
-  }
-
-void redBig4(){
-// startup();
-//deployment
-lift::motor.tarePosition();
-tilter::motor.tarePosition();
-drive::auton::resetPos();
-// lift::auton::lift(1000,200);
-intake::auton::intakeOn(-200);
-lift::pulse();
-// wait(500);
-intake::auton::intakeOn(200);
-// wait(800);
-//starting
-// drive::auton::drive(-30,-125);
-drive::auton::drive(70,150);
-drive::auton::turn(50,70);
-drive::auton::drive(100,150);
-drive::auton::turn(130,70);
-drive::auton::drive(85,150);
-drive::auton::drive(-85,-150);
-drive::auton::turn(450,-100);
-drive::auton::drive(200,150);
-drive::auton::turn(60,-70);
-//stack
-intake::auton::intakeOn(-15);
-drive::auton::drive(65,100);
-//stacking
-tilter::auton::tilter(650,75);
-intake::auton::intakeOn(-45);
-tilter::auton::tilter(850,75);
-intake::auton::intakeOff();
-tilter::auton::tilter(1050,75);
-intake::auton::intakeOn(-15);
-drive::auton::drive(-200,-115);
-tilter::motor.moveAbsolute(0,-200);
-
+void redBig()
+{
+  const int startTime = pros::millis();
+  startup();
+  drive::auton::drive(-30, -125);
+  drive::auton::drive(50, 100);
+  drive::auton::turn(40, -75);
+  // lift::motor.moveAbsolute(150,200);
+  drive::auton::driveNRE(65, 100);
+  wait(200);
+  lift::motor.moveAbsolute(2650, 200);
+  drive::auton::driveNRB(100, 200);
+  drive::auton::turn(48, 70);
+  // intake::auton::intakeOff();
+  drive::auton::drive(90, 170);
+  intake::auton::intakeOn(200);
+  // wait(300);
+  // drive::auton::drive(35,200);
+  lift::auton::lift(300, 160, startTime, 9500);
+  drive::auton::turn(260, -120);
+  lift::motor.moveAbsolute(0, 200);
+  drive::auton::driveNRE(300, 200);
+  tilter::motor.moveAbsolute(350, 50);
+  drive::auton::driveNRB(25, 200);
+  intake::auton::intakeOff();
+  //stack
+  tilter::auton::tilter(650, 95);
+  intake::auton::intakeOn(-45);
+  tilter::auton::tilter(850, 75);
+  intake::auton::intakeOff();
+  tilter::auton::tilter(1050, 50);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(-200, -85);
+  tilter::motor.moveAbsolute(0, -200);
+  lift::auton::lift(2700, 200, 14151, 1235);
 }
 
+void redBig4()
+{
+  // startup();
+  //deployment
+  lift::motor.tarePosition();
+  tilter::motor.tarePosition();
+  drive::auton::resetPos();
+  // lift::auton::lift(1000,200);
+  intake::auton::intakeOn(-200);
+  lift::pulse();
+  // wait(500);
+  intake::auton::intakeOn(200);
+  // wait(800);
+  //starting
+  // drive::auton::drive(-30,-125);
+  drive::auton::drive(70, 150);
+  drive::auton::turn(50, 70);
+  drive::auton::drive(100, 150);
+  drive::auton::turn(130, 70);
+  drive::auton::drive(85, 150);
+  drive::auton::drive(-85, -150);
+  drive::auton::turn(450, -100);
+  drive::auton::drive(200, 150);
+  drive::auton::turn(60, -70);
+  //stack
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(65, 100);
+  //stacking
+  tilter::auton::tilter(650, 75);
+  intake::auton::intakeOn(-45);
+  tilter::auton::tilter(850, 75);
+  intake::auton::intakeOff();
+  tilter::auton::tilter(1050, 75);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(-200, -115);
+  tilter::motor.moveAbsolute(0, -200);
+}
 
-void blueBig4(){
+void blueBig4()
+{
   // startup();
   //deployment
   lift::motor.tarePosition();
@@ -118,142 +121,141 @@ void blueBig4(){
   wait(300);
   //starting
   // drive::auton::drive(-30,-125);
-  drive::auton::drive(70,150);
-  drive::auton::turn(50,-70);
-  drive::auton::drive(100,150);
-  drive::auton::turn(130,-70);
-  drive::auton::drive(85,150);
-  drive::auton::drive(-85,-150);
-  drive::auton::turn(480,100);
-  drive::auton::drive(200,150);
-  drive::auton::turn(60,70);
+  drive::auton::drive(70, 150);
+  drive::auton::turn(50, -70);
+  drive::auton::drive(100, 150);
+  drive::auton::turn(130, -70);
+  drive::auton::drive(85, 150);
+  drive::auton::drive(-85, -150);
+  drive::auton::turn(480, 100);
+  drive::auton::drive(200, 150);
+  drive::auton::turn(60, 70);
   //stack
   intake::auton::intakeOn(-15);
-  drive::auton::drive(65,100);
+  drive::auton::drive(65, 100);
   //stacking
-  tilter::auton::tilter(650,75);
+  tilter::auton::tilter(650, 75);
   intake::auton::intakeOn(-45);
-  tilter::auton::tilter(850,75);
+  tilter::auton::tilter(850, 75);
   intake::auton::intakeOff();
-  tilter::auton::tilter(1050,75);
+  tilter::auton::tilter(1050, 75);
   intake::auton::intakeOn(-15);
-  drive::auton::drive(-200,-115);
-  tilter::motor.moveAbsolute(0,-200);
-
+  drive::auton::drive(-200, -115);
+  tilter::motor.moveAbsolute(0, -200);
 }
 
-
-
-void redBig5(){
+void redBig5()
+{
   startup();
-  drive::auton::drive(-30,-125);
-  drive::auton::drive(50,100);
-  drive::auton::turn(40,75);
-  drive::auton::drive(50,100);
-  drive::auton::drive(-50,-100);
-  drive::auton::turn(40,-75);
-  drive::auton::turn(40,-75);
+  drive::auton::drive(-30, -125);
+  drive::auton::drive(50, 100);
+  drive::auton::turn(40, 75);
+  drive::auton::drive(50, 100);
+  drive::auton::drive(-50, -100);
+  drive::auton::turn(40, -75);
+  drive::auton::turn(40, -75);
   // lift::motor.moveAbsolute(150,200);
-  drive::auton::drive(165,100);
-  drive::auton::turn(160,-60);
-  drive::auton::drive(350,100);
-  drive::auton::turn(90,-60);
-  drive::auton::drive(150,100);
-  lift::motor.moveAbsolute(400,200);
+  drive::auton::drive(165, 100);
+  drive::auton::turn(160, -60);
+  drive::auton::drive(350, 100);
+  drive::auton::turn(90, -60);
+  drive::auton::drive(150, 100);
+  lift::motor.moveAbsolute(400, 200);
   intake::auton::intakeOn(-200);
 }
 
-  void redSmall(){ //5 point
-    startup();
-    drive::auton::drive(1100,60);
-    intake::auton::intakeOn(50);
-    drive::auton::drive(300,-200);
-    drive::auton::turn(360,75);
-    intake::auton::intakeOn(-15);
-    drive::auton::drive(125,100);
-    //stacking
-    tilter::auton::tilter(650,75);
-    intake::auton::intakeOn(-45);
-    tilter::auton::tilter(850,75);
-    intake::auton::intakeOff();
-    tilter::auton::tilter(1050,75);
-    intake::auton::intakeOn(-15);
-    drive::auton::drive(-200,-115);
-    tilter::motor.moveAbsolute(0,-200);
-  }
-
+void redSmall()
+{ //5 point
+  startup();
+  drive::auton::drive(1100, 60);
+  intake::auton::intakeOn(50);
+  drive::auton::drive(300, -200);
+  drive::auton::turn(360, 75);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(125, 100);
+  //stacking
+  tilter::auton::tilter(650, 75);
+  intake::auton::intakeOn(-45);
+  tilter::auton::tilter(850, 75);
+  intake::auton::intakeOff();
+  tilter::auton::tilter(1050, 75);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(-200, -115);
+  tilter::motor.moveAbsolute(0, -200);
+}
 
 //blue autons
 
-  void blueBig(){
-    const int startTime = pros::millis();
-    startup();
-    drive::auton::drive(-30,-125);
-    drive::auton::drive(50,100);
-    drive::auton::turn(40,75);
-    // lift::motor.moveAbsolute(150,200);
-    drive::auton::driveNRE(75,100);
-    wait(200);
-    lift::motor.moveAbsolute(2650,200);
-    drive::auton::driveNRB(90,200);
-    drive::auton::turn(49,-70);
-    // intake::auton::intakeOff();
-    drive::auton::drive(82,150);
-    intake::auton::intakeOn(200);
-    // wait(300);
-    // drive::auton::drive(35,200);
-    lift::auton::lift(300,160,startTime,9500);
-    drive::auton::turn(300,120);
-    lift::motor.moveAbsolute(0,200);
-    drive::auton::driveNRE(300,200);
-    tilter::motor.moveAbsolute(350,50);
-    drive::auton::driveNRB(35,200);
-    intake::auton::intakeOff();
-    //stack
-    tilter::auton::tilter(650,95);
-    intake::auton::intakeOn(-45);
-    tilter::auton::tilter(850,75);
-    intake::auton::intakeOff();
-    tilter::auton::tilter(1050,50);
-    intake::auton::intakeOn(-15);
-    drive::auton::drive(-200,-85);
-    tilter::motor.moveAbsolute(0,-200);
-    lift::auton::lift(2700,200,14151,1235);
-  }
-
-void safeBlue(){
+void blueBig()
+{
   const int startTime = pros::millis();
   startup();
-  drive::auton::drive(-30,-125);
-  drive::auton::drive(50,100);
-  drive::auton::turn(40,75);
+  drive::auton::drive(-30, -125);
+  drive::auton::drive(50, 100);
+  drive::auton::turn(40, 75);
   // lift::motor.moveAbsolute(150,200);
-  drive::auton::drive(165,100);
-  drive::auton::turn(160,60);
-  drive::auton::drive(350,100);
-  drive::auton::turn(90,60);
-  drive::auton::drive(150,100);
-  lift::motor.moveAbsolute(400,200);
+  drive::auton::driveNRE(75, 100);
+  wait(200);
+  lift::motor.moveAbsolute(2650, 200);
+  drive::auton::driveNRB(90, 200);
+  drive::auton::turn(49, -70);
+  // intake::auton::intakeOff();
+  drive::auton::drive(82, 150);
+  intake::auton::intakeOn(200);
+  // wait(300);
+  // drive::auton::drive(35,200);
+  lift::auton::lift(300, 160, startTime, 9500);
+  drive::auton::turn(300, 120);
+  lift::motor.moveAbsolute(0, 200);
+  drive::auton::driveNRE(300, 200);
+  tilter::motor.moveAbsolute(350, 50);
+  drive::auton::driveNRB(35, 200);
+  intake::auton::intakeOff();
+  //stack
+  tilter::auton::tilter(650, 95);
+  intake::auton::intakeOn(-45);
+  tilter::auton::tilter(850, 75);
+  intake::auton::intakeOff();
+  tilter::auton::tilter(1050, 50);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(-200, -85);
+  tilter::motor.moveAbsolute(0, -200);
+  lift::auton::lift(2700, 200, 14151, 1235);
+}
+
+void safeBlue()
+{
+  const int startTime = pros::millis();
+  startup();
+  drive::auton::drive(-30, -125);
+  drive::auton::drive(50, 100);
+  drive::auton::turn(40, 75);
+  // lift::motor.moveAbsolute(150,200);
+  drive::auton::drive(165, 100);
+  drive::auton::turn(160, 60);
+  drive::auton::drive(350, 100);
+  drive::auton::turn(90, 60);
+  drive::auton::drive(150, 100);
+  lift::motor.moveAbsolute(400, 200);
   intake::auton::intakeOn(-200);
 }
 
-
-void safeRed(){
+void safeRed()
+{
   const int startTime = pros::millis();
   startup();
-  drive::auton::drive(-30,-125);
-  drive::auton::drive(50,100);
-  drive::auton::turn(40,-75);
+  drive::auton::drive(-30, -125);
+  drive::auton::drive(50, 100);
+  drive::auton::turn(40, -75);
   // lift::motor.moveAbsolute(150,200);
-  drive::auton::drive(165,100);
-  drive::auton::turn(160,-60);
-  drive::auton::drive(350,100);
-  drive::auton::turn(90,-60);
-  drive::auton::drive(150,100);
-  lift::motor.moveAbsolute(400,200);
+  drive::auton::drive(165, 100);
+  drive::auton::turn(160, -60);
+  drive::auton::drive(350, 100);
+  drive::auton::turn(90, -60);
+  drive::auton::drive(150, 100);
+  lift::motor.moveAbsolute(400, 200);
   intake::auton::intakeOn(-200);
 }
-
 
 /*
 const int startTime = pros::millis();
@@ -291,76 +293,78 @@ intake::auton::intakeOn(-15);
 drive::auton::drive(-200,-85);
 tilter::motor.moveAbsolute(0,-200);
 */
-  void blueSmall(){ //5 point
-    startup();
-    drive::auton::drive(1100,70);
-    intake::auton::intakeOn(50);
-    drive::auton::drive(300,-200);
-    drive::auton::turn(360,-75);
-    intake::auton::intakeOn(-15);
-       drive::auton::drive(125,100);
-    //stacking
-    tilter::auton::tilter(650,75);
-    intake::auton::intakeOn(-45);
-    tilter::auton::tilter(850,75);
-    intake::auton::intakeOff();
-    tilter::auton::tilter(1050,75);
-    intake::auton::intakeOn(-15);
-    drive::auton::drive(-200,-85);
-    tilter::motor.moveAbsolute(0,-200);
+void blueSmall()
+{ //5 point
+  startup();
+  drive::auton::drive(1100, 70);
+  intake::auton::intakeOn(50);
+  drive::auton::drive(300, -200);
+  drive::auton::turn(360, -75);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(125, 100);
+  //stacking
+  tilter::auton::tilter(650, 75);
+  intake::auton::intakeOn(-45);
+  tilter::auton::tilter(850, 75);
+  intake::auton::intakeOff();
+  tilter::auton::tilter(1050, 75);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(-200, -85);
+  tilter::motor.moveAbsolute(0, -200);
 
-
-    // tilter::auton::tilter(800,75);
-    // intake::auton::intakeOn(-45); //Phase 2: first outake going up
-    // tilter::auton::tilter(950,75);
-    // intake::auton::intakeOff(); //Phase 3: outtake off for rest of stack
-    // tilter::auton::tilter(1050,75);
-    // intake::auton::intakeOn(-15); //Phase 4: outtake to back off
-    // drive::auton::drive(-200,-85);
-    // tilter::motor.moveAbsolute(0,-200);
-  }
-
+  // tilter::auton::tilter(800,75);
+  // intake::auton::intakeOn(-45); //Phase 2: first outake going up
+  // tilter::auton::tilter(950,75);
+  // intake::auton::intakeOff(); //Phase 3: outtake off for rest of stack
+  // tilter::auton::tilter(1050,75);
+  // intake::auton::intakeOn(-15); //Phase 4: outtake to back off
+  // drive::auton::drive(-200,-85);
+  // tilter::motor.moveAbsolute(0,-200);
+}
 
 //misc autons
 
-void skills(){
-const int startTime = pros::millis();
-startup();
-drive::auton::drive(1050,60);
-drive::auton::swingTurnLeft(550,100,20);
-drive::auton::swingTurnLeft(750,100,20);
-drive::auton::drive(520,60);
-drive::auton::turn(120,-60);
-intake::auton::intakeOff();
-drive::auton::drive(450,200);
-tilter::auton::tilter(1100,60);
-intake::auton::intakeOn(200);
-wait(500);
-intake::auton::intakeOff();
-tilter::auton::tilter(1150,60);
-intake::auton::intakeOn(-15);
-drive::auton::drive(-200,-35);
-tilter::auton::tilterDown(0,-100);
+void skills()
+{
+  const int startTime = pros::millis();
+  startup();
+  drive::auton::drive(1050, 60);
+  drive::auton::swingTurnLeft(550, 100, 20);
+  drive::auton::swingTurnLeft(750, 100, 20);
+  drive::auton::drive(520, 60);
+  drive::auton::turn(120, -60);
+  intake::auton::intakeOff();
+  drive::auton::drive(450, 200);
+  tilter::auton::tilter(1100, 60);
+  intake::auton::intakeOn(200);
+  wait(500);
+  intake::auton::intakeOff();
+  tilter::auton::tilter(1150, 60);
+  intake::auton::intakeOn(-15);
+  drive::auton::drive(-200, -35);
+  tilter::auton::tilterDown(0, -100);
 }
 
-  void doNothing(){
-    startup();
-  }
-
-  void driveABit(){
-    startup();
-    drive::auton::drive(150,150);
-    drive::auton::drive(-150,-150);
-  }
-
-  void nothing(){
-
-  }
-
-  void onePoint(){
-    drive::auton::drive(300,-100);
-    drive::auton::drive(300,100);
-  }
-
-
+void doNothing()
+{
+  startup();
 }
+
+void driveABit()
+{
+  startup();
+  drive::auton::drive(150, 150);
+  drive::auton::drive(-150, -150);
+}
+
+void nothing()
+{
+}
+
+void onePoint()
+{
+  drive::auton::drive(300, -100);
+  drive::auton::drive(300, 100);
+}
+
+} // namespace auton
--- Gen6/src/custom/systems/lift.cpp
+++ Gen6/src/custom/systems/lift.cpp
@@ -3,88 +3,101 @@
 #include "custom/setup/controller.hpp"
 #include "custom/systems/lift.hpp"
 
-namespace lift{
-  Controllers controller = Controllers::NONE;
-  void up(){
-    if(BtnUp.isPressed()){
-      controller = Controllers::UP;
-    }
-    else if(controller == Controllers::UP){
-      controller = Controllers::DEINIT;
-    }
+namespace lift
+{
+Controllers controller = Controllers::NONE;
+void up()
+{
+  if (BtnUp.isPressed())
+  {
+    controller = Controllers::UP;
   }
-
-  void down(){
-    if(BtnDown.isPressed()){
-      controller = Controllers::DOWN;
-    }
-    else if(controller == Controllers::DOWN){
-      controller = Controllers::DEINIT;
-    }
+  else if (controller == Controllers::UP)
+  {
+    controller = Controllers::DEINIT;
   }
+}
 
-  void pulse(){
-    motor.moveVelocity(-125);
-    pros::delay(300);
-    motor.moveAbsolute(0,125);
-    pros::delay(300);
+void down()
+{
+  if (BtnDown.isPressed())
+  {
+    controller = Controllers::DOWN;
+  }
+  else if (controller == Controllers::DOWN)
+  {
+    controller = Controllers::DEINIT;
+  }
+}
+
+void pulse()
+{
+  motor.moveVelocity(-125);
+  pros::delay(300);
+  motor.moveAbsolute(0, 125);
+  pros::delay(300);
+  motor.moveVelocity(0);
+}
+
+void execute()
+{
+  if (motor.getPosition() < 1 && controller == Controllers::DOWN)
+  {
+    controller = Controllers::DEINIT;
+  }
+  switch (controller)
+  {
+
+  case Controllers::UP:
+    motor.moveVelocity(100);
+    break;
+
+  case Controllers::DOWN:
+    motor.moveVelocity(-100);
+
+    break;
+  case Controllers::DEINIT:
     motor.moveVelocity(0);
-  }
-
-  void execute(){
-    if(motor.getPosition() < 1 && controller == Controllers::DOWN){
-      controller = Controllers::DEINIT;
-    }
-    switch (controller)
+    if (motor.getPosition() < 40 && tilter::motor.getPosition() < 125)
     {
+      motor.moveAbsolute(40, 200);
+    }
+    controller = Controllers::NONE;
+    break;
 
-      case Controllers::UP:
-      motor.moveVelocity(100);
-      break;
+  case Controllers::NONE:
+    break;
+  }
+}
 
-      case Controllers::DOWN:
-      motor.moveVelocity(-100);
+void init()
+{
+  up();
+  down();
+  execute();
+}
 
-      break;
-      case Controllers::DEINIT:
-      motor.moveVelocity(0);
-      if(motor.getPosition() < 40 && tilter::motor.getPosition() < 125){
-      motor.moveAbsolute(40,200);
-      }
-      controller = Controllers::NONE;
-      break;
+namespace auton
+{
 
-      case Controllers::NONE:
-      break;
+void lift(int pos, int velocity, int startTime, double stopTime)
+{
+
+  if (motor.getPosition() > pos && velocity > 1)
+  {
+    velocity = -velocity;
+    while (motor.getPosition() > pos && pros::millis() - startTime < stopTime)
+    {
+      motor.moveAbsolute(pos, velocity);
     }
   }
-
-  void init(){
-    up();
-    down();
-    execute();
-  }
-
-  namespace auton{
-
-
-
-    void lift(int pos, int velocity,int startTime, double stopTime){
-
-      if(motor.getPosition() > pos && velocity > 1){
-        velocity = -velocity;
-        while(motor.getPosition() > pos && pros::millis() - startTime < stopTime){
-          motor.moveAbsolute(pos,velocity);
-
-        }
-      }
-      else{
-        while(motor.getPosition() < pos && pros::millis() - startTime < stopTime){
-          motor.moveAbsolute(pos,velocity);
-
-        }
-      }
-
+  else
+  {
+    while (motor.getPosition() < pos && pros::millis() - startTime < stopTime)
+    {
+      motor.moveAbsolute(pos, velocity);
     }
   }
 }
+} // namespace auton
+} // namespace lift

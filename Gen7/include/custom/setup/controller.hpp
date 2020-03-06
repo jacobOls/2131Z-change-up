fc7fba44f9e1cadb5cd6fd1307ffd2f24be0ead0
Jacob <swordsparring@gmail.com>
2020-03-06 14:08:18 -0700

    base lift code

--- Gen7/include/custom/setup/controller.hpp
+++ Gen7/include/custom/setup/controller.hpp
@@ -1,13 +1,28 @@
 #ifndef CONTROLLER_HPP
 #define CONTROLLER_HPP
 #include "main.h"
-
-namespace lift{
-
-extern okapi::ControllerButton btnUp;
-
+extern okapi::Controller master;
+namespace intake {
+extern okapi::ControllerButton BtnIn;
+extern okapi::ControllerButton BtnOut;
+extern okapi::ControllerButton BtnRight;
+extern okapi::ControllerButton BtnLeft;
+extern okapi::ControllerButton BtnBackUp;
+} // namespace intake
+namespace drive {
+extern okapi::ControllerButton BtnHold;
 }
 
+namespace tilter {
+extern okapi::ControllerButton BtnForward;
+extern okapi::ControllerButton BtnBackward;
+extern okapi::ControllerButton BtnReturn;
+// extern okapi::ControllerButton BtnGoUp;
+} // namespace tilter
 
-
+namespace lift {
+extern okapi::ControllerButton BtnUp;
+extern okapi::ControllerButton BtnDown;
+extern okapi::ControllerButton BtnSlowDown;
+} // namespace lift
 #endif /* end of include guard: CONTROLLER_HPP */
---
+++ Gen7/include/custom/systems/lift.hpp
@@ -0,0 +1,7 @@
+#ifndef LIFT_HPP
+#define LIFT_HPP
+namespace lift {
+enum class State { UP, DOWN, RECYCLE, DEINIT, NONE };
+void init();
+} // namespace lift
+#endif /* end of include guard: LIFT_HPP */

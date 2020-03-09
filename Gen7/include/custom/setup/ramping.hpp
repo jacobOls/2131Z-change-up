42367d3c2f25a51f1a37ec9748ca1823843874ee
Jacob <swordsparring@gmail.com>
2020-03-08 19:35:26 -0600

    exponential ramping created

--- Gen7/include/custom/setup/ramping.hpp
+++ Gen7/include/custom/setup/ramping.hpp
@@ -1,13 +1,19 @@
 #ifndef RAMPING_HPP
 #define RAMPING_HPP
-
+#include "main.h"
 class ramping {
+
+  typedef void (*rampMotor)(okapi::Motor *, int);
+
 public:
   int roc; // rate of change
   int cv;  // change value
   int min, max;
 
-  ramping(int changeRate, int changeVal, int minimum, int maximum);
+  ramping(double changeRate, double changeVal, int minimum, int maximum);
+
+  void accelMath(rampMotor handler, okapi::Motor *motor, int requested);
 };
+void accel(okapi::Motor *motor, int requested);
 
 #endif /* end of include guard: RAMPING_HPP */
--- Gen7/src/custom/setup/ramping.cpp
+++ Gen7/src/custom/setup/ramping.cpp
@@ -1,10 +1,36 @@
 #include "custom/setup/ramping.hpp"
+#include "custom/setup/motors.hpp"
+#include "custom/systems/lift.hpp"
 #include "main.h"
 
-ramping::ramping(int changeRate, int changeVal, int minimum, int maximum) {
+ramping::ramping(double changeRate, double changeVal, int minimum,
+                 int maximum) {
 
   roc = changeRate;
   cv = changeVal;
   min = minimum;
   max = maximum;
 }
+
+void ramping::accelMath(rampMotor handler, okapi::Motor *motor, int requested) {
+  int i = 1; // initial velocity
+  int t = 0; // times looped
+  double vel = 1;
+  for (vel = i; vel < requested; vel = i + cv ^ t) {
+    if (vel > requested) {
+      vel = requested;
+      break;
+    }
+    t++;
+    (*handler)(motor, requested);
+    pros::delay(roc);
+  }
+}
+
+void accel(okapi::Motor *motor, int requested) {
+  motor->moveVelocity(requested);
+}
+
+// ramping ramper = ramping(5, 5, -200, 200);
+//
+// void pie() { ramper.accelMath(accel, &lift::motor, 200); }

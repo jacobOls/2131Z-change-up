cfd58b5ece351612fa8a3650051fcc320be48507
Jacob <swordsparring@gmail.com>
2020-08-26 16:06:32 -0600

    test

--- Gen1/include/custom/systems/drive.hpp
+++ Gen1/include/custom/systems/drive.hpp
@@ -1,15 +1,8 @@
 #ifndef DRIVE_HPP
 #define DRIVE_HPP
 namespace drive {
-enum class Side { STRAIGHT, STRAFE, NONE };
-enum class State {
-  LEFTSTRAFE,
-  LEFTSTRAIGHT,
-  RIGHTSTRAFE,
-  RIGHTSTRAIGHT,
-  DEINIT,
-  NONE
-};
+enum class Left { STRAIGHT, STRAFE, NONE };
+enum class Right { STRAIGHT, STRAFE, NONE };
 void userDrive(void);
 } // namespace drive
 #endif /* end of include guard: DRIVE_HPP */
--- Gen1/src/custom/systems/drive.cpp
+++ Gen1/src/custom/systems/drive.cpp
@@ -3,16 +3,89 @@
 #include "custom/settup/motors.hpp"
 #include "main.h"
 namespace drive {
-State state = State::NONE;
-Side side = Side::NONE;
+Left left = Left::NONE;
+Right right = Right::NONE;
 
-void execute() {}
+void leftStraight() {
+  if (abs(master.getAnalog(ControllerAnalog::leftY)) >
+          abs(master.getAnalog(ControllerAnalog::leftX)) &&
+      abs(master.getAnalog(ControllerAnalog::leftY)) > 0.05) {
+    left = Left::STRAIGHT;
+  } else if (left == Left::STRAIGHT) {
+    left = Left::NONE;
+    left_drive.moveVelocity(0);
+  }
+}
+
+void rightStraight() {
+  if (abs(master.getAnalog(ControllerAnalog::rightY)) >
+          abs(master.getAnalog(ControllerAnalog::rightX)) &&
+      abs(master.getAnalog(ControllerAnalog::rightY)) > 0.05) {
+    right = Right::STRAIGHT;
+  } else if (right == Right::STRAIGHT) {
+    right = Right::NONE;
+    right_drive.moveVelocity(0);
+  }
+}
+
+void leftStrafe() {
+  if (abs(master.getAnalog(ControllerAnalog::leftX)) >
+          abs(master.getAnalog(ControllerAnalog::leftY)) &&
+      abs(master.getAnalog(ControllerAnalog::leftX)) > 0.05) {
+    left = Left::STRAFE;
+  } else if (left == Left::STRAFE) {
+    left = Left::NONE;
+    left_strafe.moveVelocity(0);
+  }
+}
+
+void rightStrafe() {
+  if (abs(master.getAnalog(ControllerAnalog::rightX)) >
+          abs(master.getAnalog(ControllerAnalog::rightY)) &&
+      abs(master.getAnalog(ControllerAnalog::rightX)) > 0.05) {
+    right = Right::STRAFE;
+  } else if (right == Right::STRAFE) {
+    right = Right::NONE;
+    right_strafe.moveVelocity(0);
+  }
+}
+
+void execute() {
+  switch (right) {
+
+  case Right::STRAIGHT:
+    right_drive.moveVelocity(master.getAnalog(ControllerAnalog::rightY) * 200);
+    break;
+
+  case Right::STRAFE:
+    right_strafe.moveVelocity(master.getAnalog(ControllerAnalog::rightX) * 200);
+
+    break;
+
+  case Right::NONE:
+    break;
+  }
+
+  switch (left) {
+
+  case Left::STRAIGHT:
+    left_drive.moveVelocity(master.getAnalog(ControllerAnalog::leftY) * 200);
+    break;
+
+  case Left::STRAFE:
+    left_strafe.moveVelocity(master.getAnalog(ControllerAnalog::leftX) * 200);
+    break;
+
+  case Left::NONE:
+    break;
+  }
+}
 void userDrive() {
   // std::cout << "init" << std::endl;
-  // leftDriveStraight();
-  // rightDriveStraight();
-  // leftStrafeDrive();
-  // rightStrafeDrive();
+  leftStraight();
+  rightStraight();
+  leftStrafe();
+  rightStrafe();
   execute();
 }
 } // namespace drive

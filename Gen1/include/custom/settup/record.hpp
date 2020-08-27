15ed5aa3d787a4521a110a64597857e24ac1bcf4
Jacob <swordsparring@gmail.com>
2020-08-27 14:52:18 -0600

    cleaning up

---
+++ Gen1/include/custom/settup/record.hpp
@@ -0,0 +1,8 @@
+#ifndef RECORD_HPP
+#define RECORD_HPP
+
+namespace record {
+void record(void);
+}
+
+#endif /* end of include guard:  */
--- Gen1/src/main.cpp
+++ Gen1/src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "custom/auton/selection.hpp"
 #include "custom/settup/motors.hpp"
+#include "custom/settup/record.hpp"
 #include "custom/systems/drive.hpp"
 #include "custom/systems/intake.hpp"
 #include "custom/systems/wheel.hpp"
@@ -13,33 +14,13 @@ void competition_initialize() {}
 
 void autonomous() { selection::execute(); }
 
-void record() {
-  FILE *txtWrite = fopen("/usd/info.txt", "a");
-  if (txtWrite != NULL) {
-    fprintf(txtWrite, "drive::left_back.moveVelocity( %f );\n",
-            drive::left_back.getActualVelocity());
-    fprintf(txtWrite, "drive::left_front.moveVelocity( %f );\n",
-            drive::left_front.getActualVelocity());
-    fprintf(txtWrite, "drive::right_back.moveVelocity( %f );\n",
-            drive::right_back.getActualVelocity());
-    fprintf(txtWrite, "drive::right_front.moveVelocity( %f );\n",
-            drive::right_front.getActualVelocity());
-    fprintf(txtWrite, "intake::intakeGroup.moveVelocity( %f );\n",
-            intake::intakeGroup.getActualVelocity());
-    fprintf(txtWrite, "wheel::wheelGroup.moveVelocity( %f ); \n",
-            wheel::wheelGroup.getActualVelocity());
-    fprintf(txtWrite, "pros::delay(20);");
-    fclose(txtWrite);
-  }
-  // pros::delay(20);\n
-}
 void opcontrol() {
 
   while (true) {
     intake::init();
     wheel::init();
     drive::userDrive();
-    record();
+    record::record();
     pros::delay(20);
   }
 }

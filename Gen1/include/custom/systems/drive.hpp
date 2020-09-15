455e512de6d87dfa5bbfda9d0bf75c077d101988
Jacob <swordsparring@gmail.com>
2020-09-15 09:32:31 -0600

    make drive auton functions public

--- Gen1/include/custom/systems/drive.hpp
+++ Gen1/include/custom/systems/drive.hpp
@@ -1,8 +1,14 @@
 #ifndef DRIVE_HPP
 #define DRIVE_HPP
+#include "main.h"
 namespace drive {
 enum class Left { STRAIGHT, STRAFE, NONE };
 enum class Right { STRAIGHT, STRAFE, NONE };
 void userDrive(void);
 } // namespace drive
+namespace auton {
+void drive(int distance, int velocity);
+void strafe(int distance, int velocity, std::string direction);
+void turn(int turnAmount, int velocity, std::string direction);
+} // namespace auton
 #endif /* end of include guard: DRIVE_HPP */

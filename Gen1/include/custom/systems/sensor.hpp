442e8ff64c6a3a52e291caa9ac6b1d1f77431f51
Jacob <swordsparring@gmail.com>
2020-09-25 14:51:31 -0600

    sensor start

---
+++ Gen1/include/custom/systems/sensor.hpp
@@ -0,0 +1,4 @@
+#ifndef SENSOR_HPP
+#define SENSOR_HPP
+namespace sensor {}
+#endif /* end of include guard: SENSOR_HPP */
---
+++ Gen1/src/custom/systems/sensor.cpp
@@ -0,0 +1,8 @@
+#include "custom/systems/sensor.hpp"
+#include "custom/settup/motors.hpp"
+#include "main.h"
+
+namespace sensor {
+pros::ADIAnalogIn intakeEntry(1);
+pros::ADIAnalogIn midElevator(2);
+} // namespace sensor

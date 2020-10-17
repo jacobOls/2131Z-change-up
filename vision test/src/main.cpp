456a11bea5d839a86d2e19cbd53bff0acac7125e
Jacob <swordsparring@gmail.com>
2020-10-17 08:06:43 -0600

    more testing

--- vision test/src/main.cpp
+++ vision test/src/main.cpp
@@ -3,22 +3,24 @@
 pros::Vision visionSensor(11);
 okapi::Motor motor(1, false, okapi::AbstractMotor::gearset::green,
                    okapi::AbstractMotor::encoderUnits::degrees);
+bool colorRed = false;
 void init() {
   visionSensor.set_wifi_mode(0);
   pros::vision_signature_s_t RED_BALL = visionSensor.signature_from_utility(
       1, 4953, 6495, 5724, -303, 259, -22, 3.000, 0);
   pros::vision_signature_s_t BLUE_BALL = visionSensor.signature_from_utility(
       2, -3551, -2285, -2918, 7295, 15009, 11152, 3.000, 0);
+  srand(time(NULL));
 }
 void execute() {
   pros::vision_object_s_t rtn = visionSensor.get_by_sig(0, 1);
-  pros::vision_object_s_t rtn2 = visionSensor.get_by_sig(0, 1);
+  pros::vision_object_s_t rtn2 = visionSensor.get_by_sig(0, 2);
   int n = visionSensor.get_object_count();
-  if (rtn.signature != 255)
-    std::cout << "sig: " << rtn.signature << std::endl;
-  // std::cout << n << std::endl;
+  std::cout << rtn.signature << std::endl;
   if (rtn.signature == 1) {
     motor.moveVelocity(200);
+  } else if (rtn2.signature == 2) {
+    motor.moveVelocity(-200);
   } else {
     motor.moveVelocity(0);
   }
@@ -35,10 +37,11 @@ void competition_initialize() {}
 void autonomous() {}
 
 void opcontrol() {
-#define SIG_1 1
+
   while (true) {
 
     execute();
+
     pros::delay(20);
   }
 }
